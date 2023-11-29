d4e012d0179324805fbee3d2a10637351416ed06
Simon Spannagel <simon.spannagel@cern.ch>
2023-11-29 08:16:48 +0100

    add licenses to test cases

--- src/constellation/protocols/CHIRP/test/test_broadcast.cpp
+++ src/constellation/protocols/CHIRP/test/test_broadcast.cpp
@@ -1,3 +1,12 @@
+/**
+ * @file
+ * @brief Tests for CHIRP broadcast receiver
+ *
+ * @copyright Copyright (c) 2023 DESY and the Constellation authors.
+ * This software is distributed under the terms of the EUPL-1.2 License, copied verbatim in the file "LICENSE.md".
+ * SPDX-License-Identifier: EUPL-1.2
+ */
+
 #include <chrono>
 #include <iostream>
 #include <future>
