acd1073e35ea3c04266f24299ed552126b30a064
Simon Spannagel <simon.spannagel@cern.ch>
2023-11-29 08:13:22 +0100

    Add license info to CHIPR implementation

--- src/constellation/protocols/CHIRP/exceptions.hpp
+++ src/constellation/protocols/CHIRP/exceptions.hpp
@@ -1,3 +1,12 @@
+/**
+ * @file
+ * @brief CHIRP protocol exceptions
+ *
+ * @copyright Copyright (c) 2023 DESY and the Constellation authors.
+ * This software is distributed under the terms of the EUPL-1.2 License, copied verbatim in the file "LICENSE.md".
+ * SPDX-License-Identifier: EUPL-1.2
+ */
+
 #pragma once
 
 #include <exception>
