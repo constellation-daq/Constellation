72215eead77b55a808407074241bd71f0e16d88a
Simon Spannagel <simon.spannagel@cern.ch>
2023-12-16 17:18:05 +0100

    Add MessageHeader files - top be refactored inot global classes

---
+++ src/constellation/core/logging/MessageHeader.cpp
@@ -0,0 +1,58 @@
+#include "MessageHeader.hpp"
+
+#include <iostream>
+
+MessageHeader::MessageHeader(void* data, std::size_t size) {
+    // Offset since we decode four separate msgpack objects
+    std::size_t offset = 0;
+
+    // Unpack protocol
+    const auto msgpack_protocol = msgpack::unpack(static_cast<char*>(data), size, offset);
+    const auto protocol = msgpack_protocol->as<std::string>();
+    if (protocol != CMDP1_PROTOCOL) {
+        // Not CMDP version 1 message
+        // TODO: throw
+    }
+
+    // Unpack sender
+    const auto msgpack_sender = msgpack::unpack(static_cast<char*>(data), size, offset);
+    sender_ = msgpack_sender->as<std::string>();
+
+    // Unpack time
+    const auto msgpack_time = msgpack::unpack(static_cast<char*>(data), size, offset);
+    time_ = msgpack_time->as<std::chrono::system_clock::time_point>();
+
+    // Unpack tags
+    const auto msgpack_tags = msgpack::unpack(static_cast<char*>(data), size, offset);
+    tags_ = msgpack_tags->as<std::map<std::string, msgpack::type::variant>>();
+}
+
+msgpack::sbuffer MessageHeader::assemble() const {
+    msgpack::sbuffer sbuf {};
+    msgpack::packer<msgpack::sbuffer> packer {&sbuf};
+
+    // first pack version
+    packer.pack(CMDP1_PROTOCOL);
+    // then sender
+    packer.pack(sender_);
+    // then time
+    packer.pack(time_);
+    // then tags
+    packer.pack(tags_);
+
+    // content can be accessed via .data() and .size()
+    return sbuf;
+}
+
+// TODO: we probably want this as a stream
+void MessageHeader::print() const {
+    std::cout << "Header: CMDP1\n"
+              << "Sender: " << sender_ << "\n"
+              << "Time:   " << time_ << "\n"
+              << "Tags:\n";
+    for (const auto& entry : tags_) {
+        // TODO: second part should probably be in a try / catch block?
+        std::cout << " " << entry.first << ": " << entry.second.as_string() << "\n";
+    }
+    std::cout << std::flush;
+}
---
+++ src/constellation/core/logging/MessageHeader.hpp
@@ -0,0 +1,42 @@
+#pragma once
+
+#include <chrono>
+#include <map>
+#include <string>
+#include <string_view>
+
+#include <msgpack.hpp> // https://github.com/msgpack/msgpack-c/blob/cpp_master/
+
+using namespace std::literals::string_view_literals;
+
+constexpr std::string_view CMDP1_PROTOCOL = "CMDP\01"sv;
+
+// Note: we might want to have the protocol as a template argument for the class if we reuse it for CDTP as well
+
+class MessageHeader {
+public:
+    MessageHeader(std::string_view sender, std::chrono::system_clock::time_point time) : sender_(sender), time_(time) {}
+    MessageHeader(std::string_view sender) : sender_(sender), time_(std::chrono::system_clock::now()) {}
+
+    // Reconstruct from bytes
+    MessageHeader(void* data, std::size_t size);
+
+    std::chrono::system_clock::time_point getTime() const { return time_; }
+    std::string_view getSender() const { return sender_; }
+    std::map<std::string, msgpack::type::variant> getTags() const { return tags_; }
+
+    template<typename T>
+    T getTag(const std::string& key) const { return tags_.at(key); }
+
+    template<typename T>
+    void setTag(const std::string& key, T value) { tags_[key] = value; }
+
+    msgpack::sbuffer assemble() const;
+
+    void print() const;
+
+private:
+    std::string sender_;
+    std::chrono::system_clock::time_point time_;
+    std::map<std::string, msgpack::type::variant> tags_;
+};
