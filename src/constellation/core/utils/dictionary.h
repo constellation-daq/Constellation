126c26b54623dc0375dddbd4eadb10267cb3d379
Simon Spannagel <simon.spannagel@desy.de>
2023-12-19 17:21:26 +0100

    Move dictionary to separate header

---
+++ src/constellation/core/utils/dictionary.h
@@ -0,0 +1,20 @@
+/**
+ * @file
+ * @brief Dictionary type
+ *
+ * @copyright Copyright (c) 2023 DESY and the Constellation authors.
+ * This software is distributed under the terms of the EUPL-1.2 License, copied verbatim in the file "LICENSE.md".
+ * SPDX-License-Identifier: EUPL-1.2
+ */
+
+#pragma once
+
+#include <chrono>
+#include <map>
+#include <optional>
+#include <string>
+
+namespace Constellation {
+    using dictionary_t =
+    std::map<std::string, std::variant<size_t, bool, int, std::optional<double>, std::string, std::chrono::system_clock::time_point>>;
+}
