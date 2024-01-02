9f5ce5669ee647a592c2aaab1e7c9c2b5513c4dc
Simon Spannagel <simon.spannagel@cern.ch>
2024-01-02 12:15:31 +0100

    Simplify time printing necessary for some compiler versions

--- src/constellation/core/utils/std23.hpp
+++ src/constellation/core/utils/std23.hpp
@@ -37,20 +37,16 @@ namespace std {
 #endif
 
 #ifndef __cpp_lib_format
+#include <ctime>
+#include <iomanip>
+#include <time.h>
 namespace std {
     template <typename Clock, typename Duration>
     std::ostream& operator<<(std::ostream& stream, const std::chrono::time_point<Clock, Duration>& time_point) {
-        const time_t time = Clock::to_time_t(time_point);
-#if __GNUC__ > 4 || ((__GNUC__ == 4) && __GNUC_MINOR__ > 8 && __GNUC_REVISION__ > 1)
-        struct tm tm;
-        localtime_r(&time, &tm);
+        const auto tt = Clock::to_time_t(time_point);
+        std::tm tm {};
+        localtime_r(&tt, &tm); // there is no thread-safe std::locatime
         return stream << std::put_time(&tm, "%c");
-#else
-        char buffer[26];
-        ctime_r(&time, buffer);
-        buffer[24] = '\0';
-        return stream << buffer;
-#endif
     }
 } // namespace std
 #endif
