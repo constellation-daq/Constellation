726252641989dbb9bb51c59f2e9b6312e7a5d01c
Stephan Lachnit <stephanlachnit@debian.org>
2024-01-28 13:24:45 +0100

    Fix some coverity performance defects
    Signed-off-by: Stephan Lachnit <stephanlachnit@debian.org>

--- src/constellation/core/logging/test/cmdp_log_send.cpp
+++ src/constellation/core/logging/test/cmdp_log_send.cpp
@@ -29,7 +29,7 @@ int main(int argc, char* argv[]) {
     SinkManager::getInstance().setGlobalConsoleLevel(OFF);
     SinkManager::getInstance().setCMDPLevelsCustom(TRACE);
 
-    Logger logger {topic};
+    Logger logger {std::move(topic)};
     std::cout << "Starting logging on port " << SinkManager::getInstance().getCMDPPort() << std::endl;
 
     while(true) {
