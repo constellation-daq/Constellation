555b32c935ae0cc1860ba8ee5f951b42d058632a
Joel Ekström <joel.ekstrom@dvel.se>
2024-02-28 09:51:21 +0100

    style: change name of flatten_dict to pack_config

--- python/constellation/confighandler.py
+++ python/constellation/confighandler.py
@@ -11,13 +11,13 @@ import logging
 logger = logging.getLogger(__name__)
 
 
-def flatten_dict(dictionary, parent_key="", separator="."):
-    """Help-function for flattening dictionaries"""
+def pack_config(dictionary, parent_key="", separator="."):
+    """Pack config dict into a flat dict"""
     items = []
     for key, value in dictionary.items():
         new_key = parent_key + separator + key if parent_key else key
         if isinstance(value, MutableMapping):
-            items.extend(flatten_dict(value, new_key, separator=separator).items())
+            items.extend(pack_config(value, new_key, separator=separator).items())
         else:
             items.append((new_key, value))
     return dict(items)
