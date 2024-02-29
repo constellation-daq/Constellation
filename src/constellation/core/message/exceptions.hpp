196e05cb63fe87df9cd98b0e1256e324e8e053b8
Simon Spannagel <simon.spannagel@desy.de>
2024-02-29 12:11:41 +0100

    Add first message exceptions

---
+++ src/constellation/core/message/exceptions.hpp
@@ -0,0 +1,62 @@
+/**
+ * @file
+ * @brief Collection of all message exceptions
+ *
+ * @copyright Copyright (c) 2024 DESY and the Constellation authors.
+ * This software is distributed under the terms of the EUPL-1.2 License, copied verbatim in the file "LICENSE.md".
+ * SPDX-License-Identifier: EUPL-1.2
+ */
+
+#pragma once
+
+#include <string_view>
+
+#include "constellation/core/utils/exceptions.hpp"
+
+namespace constellation::message {
+    /**
+     * @ingroup Exceptions
+     * @brief Error in decoding the message
+     *
+     * The message cannot be correctly decoded because the format does not adhere to protocol.
+     */
+    class MessageDecodingError : public utils::RuntimeError {
+    public:
+        explicit MessageDecodingError(std::string_view msg) {
+            error_message_ = "Error in decoding message ";
+            error_message_ += msg;
+        }
+    };
+
+    /**
+     * @ingroup Exceptions
+     * @brief Invalid protocol identifier
+     *
+     * The message identifier does not represent a valid or known protocol identifier
+     */
+    class InvalidProtocolError : public utils::RuntimeError {
+    public:
+        explicit InvalidProtocolError(std::string_view protocol) {
+            error_message_ = "Invalid protocol identifier \"";
+            error_message_ += protocol;
+            error_message_ += "\"";
+        }
+    };
+
+    /**
+     * @ingroup Exceptions
+     * @brief Unexpected protocol identifier
+     *
+     * The protocol identifier of the message does not match the expected protocol
+     */
+    class UnexpectedProtocolError : public utils::RuntimeError {
+    public:
+        explicit UnexpectedProtocolError(std::string_view prot_recv, std::string_view prot_exp) {
+            error_message_ = "Received protocol \"";
+            error_message_ += prot_recv;
+            error_message_ += "\" does not match expected identifier \"";
+            error_message_ += prot_exp;
+            error_message_ += "\"";
+        }
+    };
+} // namespace constellation::message
