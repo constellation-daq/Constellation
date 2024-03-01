53a1617055f1e7176394fb790f68208b4fff8de6
Hanno Perrey <hanno.perrey@dvel.se>
2024-03-01 15:35:18 +0100

    refactorize pytests: mv fixtures to conftest.py

--- python/tests/conftest.py
+++ python/tests/conftest.py
@@ -5,13 +5,22 @@ SPDX-License-Identifier: CC-BY-4.0
 
 import pytest
 from unittest.mock import patch, MagicMock
+import zmq
 
 from constellation.chirp import (
     CHIRP_PORT,
     CHIRPBeaconTransmitter,
 )
 
+from constellation.cscp import CommandTransmitter
+
+# chirp
 mock_chirp_packet_queue = []
+
+# satellite
+mock_packet_queue_recv = {}
+mock_packet_queue_sender = {}
+send_port = 11111
 
 
 # SIDE EFFECTS
@@ -44,4 +53,79 @@ def mock_chirp_transmitter(mock_chirp_socket):
         "mock_sender",
         "mockstellation",
     )
+    yield t
+
+
+class mocket:
+    """Mock socket for a receiver."""
+
+    def __init__(self):
+        self.port = 0
+
+    # receiver
+    def send(self, payload, flags=None):
+        """Append buf to queue."""
+        try:
+            mock_packet_queue_sender[self.port].append(payload)
+        except KeyError:
+            mock_packet_queue_sender[self.port] = [payload]
+
+    def send_string(self, payload, flags=None):
+        self.send(payload)
+
+    def recv_multipart(self, flags=None):
+        """Pop entry from queue."""
+        if (
+            self.port not in mock_packet_queue_recv
+            or not mock_packet_queue_recv[self.port]
+        ):
+            raise zmq.ZMQError("no mock data")
+        # "pop all"
+        r, mock_packet_queue_recv[self.port][:] = (
+            mock_packet_queue_recv[self.port][:],
+            [],
+        )
+        return r
+
+    def bind(self, host):
+        self.port = int(host.split(":")[2])
+        print(f"Bound Mocket on {self.port}")
+
+
+# SIDE EFFECTS SENDER
+def mock_sock_send_sender(payload, flags=None):
+    """Append buf to queue."""
+    try:
+        mock_packet_queue_recv[send_port].append(payload)
+    except KeyError:
+        mock_packet_queue_recv[send_port] = [payload]
+
+
+def mock_sock_recv_multipart_sender(flags=None):
+    """Pop entry from queue."""
+    if (
+        send_port not in mock_packet_queue_sender
+        or not mock_packet_queue_sender[send_port]
+    ):
+        raise zmq.ZMQError("no mock data")
+    # "pop all"
+    r, mock_packet_queue_sender[send_port][:] = (
+        mock_packet_queue_sender[send_port][:],
+        [],
+    )
+    return r
+
+
+@pytest.fixture
+def mock_socket_sender():
+    mock = MagicMock()
+    mock = mock.return_value
+    mock.send = MagicMock(side_effect=mock_sock_send_sender)
+    mock.recv_multipart = MagicMock(side_effect=mock_sock_recv_multipart_sender)
+    yield mock
+
+
+@pytest.fixture
+def mock_cmd_transmitter(mock_socket_sender):
+    t = CommandTransmitter("mock_sender", mock_socket_sender)
     yield t
--- python/tests/test_satellite.py
+++ python/tests/test_satellite.py
@@ -8,7 +8,6 @@ import pytest
 import time
 import threading
 from unittest.mock import MagicMock, patch
-import zmq
 
 from constellation.cscp import CSCPMessageVerb, CommandTransmitter
 
@@ -24,89 +23,7 @@ from constellation.chirp import (
     CHIRPServiceIdentifier,
 )
 
-from conftest import mock_chirp_packet_queue
-
-chirp_offer_data_666 = b"\x96\xa9CHIRP%x01\x02\xc4\x10\xe8\x05\x01\xfc\x10\x00Z\xfa\x8c\xfa\x96\x8c\xa4\xb5\x13j\xc4\x10,\x13>\x81\xd4&Z2\xa3\xae\xa6\x9c\xc5aS\x82\x04\xcd\x02\x9a"
-
-mock_packet_queue_recv = {}
-mock_packet_queue_sender = {}
-send_port = 11111
-
-
-# SIDE EFFECTS
-class mocket:
-    """Mock socket for a receiver."""
-
-    def __init__(self):
-        self.port = 0
-
-    # receiver
-    def send(self, payload, flags=None):
-        """Append buf to queue."""
-        try:
-            mock_packet_queue_sender[self.port].append(payload)
-        except KeyError:
-            mock_packet_queue_sender[self.port] = [payload]
-
-    def send_string(self, payload, flags=None):
-        self.send(payload)
-
-    def recv_multipart(self, flags=None):
-        """Pop entry from queue."""
-        if (
-            self.port not in mock_packet_queue_recv
-            or not mock_packet_queue_recv[self.port]
-        ):
-            raise zmq.ZMQError("no mock data")
-        # "pop all"
-        r, mock_packet_queue_recv[self.port][:] = (
-            mock_packet_queue_recv[self.port][:],
-            [],
-        )
-        return r
-
-    def bind(self, host):
-        self.port = int(host.split(":")[2])
-        print(f"Bound Mocket on {self.port}")
-
-
-# SIDE EFFECTS SENDER
-def mock_sock_send_sender(payload, flags=None):
-    """Append buf to queue."""
-    try:
-        mock_packet_queue_recv[send_port].append(payload)
-    except KeyError:
-        mock_packet_queue_recv[send_port] = [payload]
-
-
-def mock_sock_recv_multipart_sender(flags=None):
-    """Pop entry from queue."""
-    if (
-        send_port not in mock_packet_queue_sender
-        or not mock_packet_queue_sender[send_port]
-    ):
-        raise zmq.ZMQError("no mock data")
-    # "pop all"
-    r, mock_packet_queue_sender[send_port][:] = (
-        mock_packet_queue_sender[send_port][:],
-        [],
-    )
-    return r
-
-
-@pytest.fixture
-def mock_socket_sender():
-    mock = MagicMock()
-    mock = mock.return_value
-    mock.send = MagicMock(side_effect=mock_sock_send_sender)
-    mock.recv_multipart = MagicMock(side_effect=mock_sock_recv_multipart_sender)
-    yield mock
-
-
-@pytest.fixture
-def mock_cmd_transmitter(mock_socket_sender):
-    t = CommandTransmitter("mock_sender", mock_socket_sender)
-    yield t
+from conftest import mock_chirp_packet_queue, mocket
 
 
 @pytest.fixture
@@ -174,9 +91,9 @@ def test_satellite_unknown_cmd_recv(mock_socket_sender, mock_satellite):
 
 
 @pytest.mark.forked
-def test_satellite_fsm_change_on_cmd(mock_socket_sender, mock_satellite):
+def test_satellite_fsm_change_on_cmd(mock_cmd_transmitter, mock_satellite):
     """Test cmd reception."""
-    sender = CommandTransmitter("mock_sender", mock_socket_sender)
+    sender = mock_cmd_transmitter
     # send a request
     sender.send_request("get_state")
     time.sleep(0.2)
