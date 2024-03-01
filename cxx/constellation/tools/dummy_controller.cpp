34e621cff07680747a78cccf45d62690be6c3893
Stephan Lachnit <stephanlachnit@debian.org>
2024-03-01 14:48:35 +0100

    Add satellite implementation
    Signed-off-by: Stephan Lachnit <stephanlachnit@debian.org>

---
+++ cxx/constellation/tools/dummy_controller.cpp
@@ -0,0 +1,52 @@
+/**
+ * @file
+ * @brief Dummy controller
+ *
+ * @copyright Copyright (c) 2024 DESY and the Constellation authors.
+ * This software is distributed under the terms of the EUPL-1.2 License, copied verbatim in the file "LICENSE.md".
+ * SPDX-License-Identifier: EUPL-1.2
+ */
+
+#include <iostream>
+#include <string>
+
+#include <zmq.hpp>
+#include <zmq_addon.hpp>
+
+#include "constellation/core/message/CSCP1Message.hpp"
+#include "constellation/core/message/CSIG_definitions.hpp"
+#include "constellation/core/utils/casts.hpp"
+
+using namespace constellation::message;
+using namespace constellation::utils;
+
+int main(int argc, char* argv[]) {
+    // Get address via cmdline
+    if(argc != 2) {
+        std::cout << "Invalid usage: dummy_controller ZMQ_ENDPOINT" << std::endl;
+    }
+
+    zmq::context_t context {};
+    zmq::socket_t req {context, zmq::socket_type::req};
+
+    req.connect(argv[1]);
+
+    while(true) {
+        std::string command;
+        std::cout << "Send command: ";
+        std::getline(std::cin, command);
+
+        // Send command
+        auto send_msg = CSCP1Message({"dummy_controller"}, {CSCP1Message::Type::REQUEST, std::move(command)});
+        send_msg.assemble().send(req);
+
+        // Receive reply
+        zmq::multipart_t recv_zmq_msg {};
+        recv_zmq_msg.recv(req);
+        auto recv_msg = CSCP1Message::disassemble(recv_zmq_msg);
+
+        // Print message
+        std::cout << recv_msg.getHeader().to_string() << "\n"
+                  << "Verb: " << to_string(recv_msg.getVerb().first) << " : " << recv_msg.getVerb().second << std::endl;
+    }
+}
---
+++ cxx/constellation/tools/dummy_satellite.cpp
@@ -0,0 +1,47 @@
+/**
+ * @file
+ * @brief Dummy satellite
+ *
+ * @copyright Copyright (c) 2024 DESY and the Constellation authors.
+ * This software is distributed under the terms of the EUPL-1.2 License, copied verbatim in the file "LICENSE.md".
+ * SPDX-License-Identifier: EUPL-1.2
+ */
+
+#include <memory>
+#include <string>
+
+#include <asio.hpp>
+
+#include "constellation/core/chirp/Manager.hpp"
+#include "constellation/core/logging/SinkManager.hpp"
+#include "constellation/satellite/Satellite.hpp"
+#include "constellation/satellite/SatelliteImplementation.hpp"
+
+using namespace asio::ip;
+using namespace constellation;
+using namespace constellation::satellite;
+
+class DummySatellite : public Satellite {
+public:
+    DummySatellite() = default;
+};
+
+int main(int argc, char* argv[]) {
+    // Get satellite name via cmdline
+    std::string satellite_name = host_name();
+    if(argc >= 2) {
+        satellite_name = argv[1];
+    }
+
+    // Create CHIRP manager and set as default
+    auto chirp_manager = chirp::Manager(address_v4::broadcast(), address_v4::any(), "cnstln1", satellite_name);
+    chirp_manager.setAsDefaultInstance();
+
+    // Create and start satellite
+    auto satellite = std::make_shared<DummySatellite>();
+    auto satellite_implementation = SatelliteImplementation(satellite_name, satellite);
+    satellite_implementation.start();
+
+    // Wait for interrupt
+    satellite_implementation.join();
+}
