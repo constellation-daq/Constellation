79fc296aaa1e305bcbd70c4bf001a5fdf0f0973f
Hanno Perrey <hanno.perrey@dvel.se>
2024-03-13 16:10:12 +0100

    refactorize mock fixtures for sockets

--- python/tests/conftest.py
+++ python/tests/conftest.py
@@ -5,6 +5,8 @@ SPDX-License-Identifier: CC-BY-4.0
 
 import pytest
 from unittest.mock import patch, MagicMock
+import operator
+import time
 import zmq
 
 from constellation.chirp import (
@@ -56,19 +58,31 @@ def mock_chirp_transmitter(mock_chirp_socket):
     yield t
 
 
-class mocket:
+class mocket(MagicMock):
     """Mock socket for a receiver."""
 
-    def __init__(self):
+    def __init__(self, *args, **kwargs):
+        super().__init__()
         self.port = 0
+        self.endpoint = 0  # 0 or 1
 
-    # receiver
+    def _get_queue(self, out: bool):
+        """Flip what queue to use depending on direction and endpoint.
+
+        Makes sure that A sends on B's receiving queue and vice-versa.
+
+        """
+        if operator.xor(self.endpoint, out):
+            return mock_packet_queue_sender
+        else:
+            return mock_packet_queue_recv
+
     def send(self, payload, flags=None):
         """Append buf to queue."""
         try:
-            mock_packet_queue_sender[self.port].append(payload)
+            self._get_queue(True)[self.port].append(payload)
         except KeyError:
-            mock_packet_queue_sender[self.port] = [payload]
+            self._get_queue(True)[self.port] = [payload]
 
     def send_string(self, payload, flags=None):
         self.send(payload.encode())
@@ -76,63 +90,50 @@ class mocket:
     def recv_multipart(self, flags=None):
         """Pop entry from queue."""
         if (
-            self.port not in mock_packet_queue_recv
-            or not mock_packet_queue_recv[self.port]
+            self.port not in self._get_queue(False)
+            or not self._get_queue(False)[self.port]
         ):
             raise zmq.ZMQError("no mock data")
         # "pop all"
-        r, mock_packet_queue_recv[self.port][:] = (
-            mock_packet_queue_recv[self.port][:],
+        r, self._get_queue(False)[self.port][:] = (
+            self._get_queue(False)[self.port][:],
             [],
         )
         return r
+
+    def recv(self, flags=None):
+        """Pop single entry from queue."""
+        if flags == zmq.NOBLOCK:
+            if (
+                send_port not in self._get_queue(False)
+                or not self._get_queue(False)[send_port]
+            ):
+                raise zmq.ZMQError("no mock data")
+            return self._get_queue(False)[send_port].pop(0)
+        else:
+            # block
+            while (
+                send_port not in self._get_queue(False)
+                or not self._get_queue(False)[send_port]
+            ):
+                time.sleep(0.01)
+            return self._get_queue(False)[send_port].pop(0)
 
     def bind(self, host):
         self.port = int(host.split(":")[2])
         print(f"Bound Mocket on {self.port}")
 
-
-# SIDE EFFECTS SENDER
-def mock_sock_send_sender(payload, flags=None):
-    """Append buf to queue."""
-    try:
-        mock_packet_queue_recv[send_port].append(payload)
-    except KeyError:
-        mock_packet_queue_recv[send_port] = [payload]
-
-
-def mock_sock_recv_multipart_sender(flags=None):
-    """Pop entry from queue."""
-    if (
-        send_port not in mock_packet_queue_sender
-        or not mock_packet_queue_sender[send_port]
-    ):
-        raise zmq.ZMQError("no mock data")
-    # "pop all"
-    r, mock_packet_queue_sender[send_port][:] = (
-        mock_packet_queue_sender[send_port][:],
-        [],
-    )
-    return r
-
-
-def mock_sock_recv_sender(flags=None):
-    """Pop single entry from queue."""
-    if (
-        send_port not in mock_packet_queue_sender
-        or not mock_packet_queue_sender[send_port]
-    ):
-        raise zmq.ZMQError("no mock data")
-    return mock_packet_queue_sender[send_port].pop(0)
+    def connect(self, host):
+        self.port = int(host.split(":")[2])
+        print(f"Bound Mocket on {self.port}")
 
 
 @pytest.fixture
 def mock_socket_sender():
-    mock = MagicMock()
-    mock = mock.return_value
-    mock.send = MagicMock(side_effect=mock_sock_send_sender)
-    mock.recv_multipart = MagicMock(side_effect=mock_sock_recv_multipart_sender)
-    mock.recv = MagicMock(side_effect=mock_sock_recv_sender)
+    mock = mocket()
+    mock.return_value = mock
+    mock.endpoint = 1
+    mock.port = send_port
     yield mock
 
 
