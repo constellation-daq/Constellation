3b3e31266fa83840a5589acc80cd6e418d4a62e1
Stephan Lachnit <stephanlachnit@debian.org>
2024-03-18 13:51:48 +0100

    caribou: make it work^TM
    Signed-off-by: Stephan Lachnit <stephanlachnit@debian.org>

--- cxx/satellites/Caribou/CaribouSatellite.cpp
+++ cxx/satellites/Caribou/CaribouSatellite.cpp
@@ -9,82 +9,105 @@
 
 #include "CaribouSatellite.hpp"
 
+#include <algorithm>
 #include <chrono>
+#include <cstdint>
+#include <fstream>
+#include <memory>
+#include <mutex>
+#include <stop_token>
+#include <string>
+#include <string_view>
 #include <thread>
 
 // Caribou Peary includes
-#include "utils/configuration.hpp"
+#include <peary/device/DeviceManager.hpp>
+#include <peary/utils/configuration.hpp>
+#include <peary/utils/exceptions.hpp>
 
 #include "constellation/core/logging/log.hpp"
+#include "constellation/satellite/Satellite.hpp"
 
 using namespace caribou;
 using namespace std::literals::chrono_literals;
 
-CaribouSatellite::CaribouSatellite(std::string_view name) : Satellite(name) {
+// generator function for loading satellite from shared library
+extern "C" std::shared_ptr<Satellite> generator(std::string_view type_name, std::string_view satellite_name) {
+    return std::make_shared<CaribouSatellite>(type_name, satellite_name);
+}
 
-    // Separate the device name from the device identifier:
-    auto const pos = name.find_last_of('_');
-    if(pos == std::string::npos) {
-        name_ = name;
-        LOG(logger_, INFO) << "Instantiated CaribouProducer for device \"" << name << "\"";
+CaribouSatellite::CaribouSatellite(std::string_view type_name, std::string_view satellite_name)
+    : Satellite(type_name, satellite_name) {
+    const auto pos = satellite_name.find_last_of('_');
+    if(pos == std::string_view::npos) {
+        device_name_ = satellite_name;
+        LOG(logger_, INFO) << "Instantiated " << getCanonicalName() << " for device \"" << device_name_ << "\"";
     } else {
-        name_ = name.substr(0, pos);
-        const auto identifier = name.substr(pos + 1);
-        LOG(logger_, INFO) << "Instantiated CaribouProducer for device \"" << name << "\" with identifier \"" << identifier
-                           << "\"";
+        device_name_ = satellite_name.substr(0, pos);
+        const auto identifier = satellite_name.substr(pos + 1);
+        LOG(logger_, INFO) << "Instantiated " << getCanonicalName() << " for device \"" << device_name_
+                           << "\" with identifier \"" << identifier << "\"";
     }
 
     // Create new Peary device manager
-    manager_ = new DeviceManager();
+    manager_ = std::make_shared<DeviceManager>();
 }
 
-void CaribouSatellite::initializing(const std::stop_token& stop_token, const std::any& config) {
-    LOG(logger_, INFO) << "Initialising CaribouProducer";
-    auto ini = GetInitConfiguration();
+void CaribouSatellite::initializing(const std::stop_token& /*stop_token*/, const std::any& /*config*/) {
+    LOG(logger_, INFO) << "Initializing " << getCanonicalName();
 
-    // Open configuration file and create object:
-    caribou::Configuration config;
-    auto confname = ini->Get("config_file", "");
-    std::ifstream file(confname);
-    LOG(logger_, INFO) << "Attempting to use initial device configuration \"" << confname << "\"";
-    if(!file.is_open()) {
-        LOG(logger_, CRITICAL) << "No configuration file provided.";
+    // Open configuration file and read caribou configuration
+    caribou::Configuration caribou_config {};
+    const auto config_file_path = std::string("config.txt"); // FIXME config->get("config_file");
+    LOG(logger_, INFO) << "Attempting to use initial device configuration \"" << config_file_path << "\"";
+    std::ifstream config_file {config_file_path};
+    if(!config_file.is_open()) {
+        LOG(logger_, CRITICAL) << "Could not open configuration file \"" << config_file_path << "\"";
     } else {
-        config = caribou::Configuration(file);
+        caribou_config = caribou::Configuration(config_file);
     }
 
-    // Select section from the configuration file relevant for this device:
-    auto sections = config.GetSections();
-    if(std::find(sections.begin(), sections.end(), name_) != sections.end()) {
-        config.SetSection(name_);
+    // Select section from the configuration file relevant for this device
+    const auto sections = caribou_config.GetSections();
+    if(std::find(sections.cbegin(), sections.cend(), device_name_) != sections.cend()) {
+        caribou_config.SetSection(device_name_);
     }
 
     std::lock_guard<std::mutex> lock {device_mutex_};
-    size_t device_id = manager_->addDevice(name_, config);
-    LOG(logger_, INFO) << "Manager returned device ID " << device_id << ", fetching device...";
-    device_ = manager_->getDevice(device_id);
-
-    // Add secondary device if it is configured:
-    if(ini->Has("secondary_device")) {
-        std::string secondary = ini->Get("secondary_device", std::string());
-        if(std::find(sections.begin(), sections.end(), secondary) != sections.end()) {
-            config.SetSection(secondary);
-        }
-        size_t device_id2 = manager_->addDevice(secondary, config);
-        LOG(logger_, INFO) << "Manager returned device ID " << std::to_string(device_id2)
-                           << ", fetching secondary device...";
-        secondary_device_ = manager_->getDevice(device_id2);
+    try {
+        const auto device_id = manager_->addDevice(device_name_, caribou_config);
+        LOG(logger_, INFO) << "Manager returned device ID " << device_id << ", fetching device...";
+        device_ = manager_->getDevice(device_id);
+    } catch(const caribou::DeviceException& error) {
+        LOG(logger_, CRITICAL) << "Failed to get device \"" << device_name_ << "\": " << error.what();
+        throw std::exception();
     }
+
+    // Add secondary device if it is configured
+    if(false /*FIXME config->has("secondary_device")*/) {
+        const auto secondary = std::string(); // FIXME config->get("secondary_device", std::string());
+        if(std::find(sections.begin(), sections.end(), secondary) != sections.end()) {
+            caribou_config.SetSection(secondary);
+        }
+        try {
+            const auto device_id2 = manager_->addDevice(secondary, caribou_config);
+            LOG(logger_, INFO) << "Manager returned device ID " << device_id2 << ", fetching secondary device...";
+            secondary_device_ = manager_->getDevice(device_id2);
+        } catch(const caribou::DeviceException& error) {
+            LOG(logger_, CRITICAL) << "Failed to get secondary device \"" << secondary << "\": " << error.what();
+            throw std::exception();
+        }
+    }
+
+    LOG(logger_, STATUS) << getCanonicalName() << " initialized";
 }
 
-void CaribouSatellite::launching(const std::stop_token& stop_token) {
-    auto config = GetConfiguration();
-    LOG(logger_, INFO) << "Configuring CaribouProducer: " << config->Name();
-
-    std::lock_guard<std::mutex> lock {device_mutex_};
+void CaribouSatellite::launching(const std::stop_token& /*stop_token*/) {
     LOG(logger_, INFO) << "Configuring device " << device_->getName();
 
-    // Switch on the device power:
+    std::lock_guard<std::mutex> lock {device_mutex_};
+
+    // Switch on the device power
     device_->powerOn();
 
     if(secondary_device_ != nullptr) {
@@ -102,106 +125,113 @@ void CaribouSatellite::launching(const std::stop_token& stop_token) {
     }
 
     // Set additional registers from the configuration:
-    if(config->Has("register_key") || config->Has("register_value")) {
-        auto key = config->Get("register_key", "");
-        auto value = config->Get("register_value", 0);
+    /*if(config_->has("register_key") || config_->has("register_value")) {
+        auto key = config_->get("register_key", "");
+        auto value = config_->get("register_value", 0);
         device_->setRegister(key, value);
         LOG(logger_, INFO) << "Setting " << key << " = " << std::to_string(value);
-    }
+    }*/
 
     // Select which ADC signal to regularly fetch:
-    adc_signal_ = config->Get("adc_signal", "");
-    adc_freq_ = config->Get("adc_frequency", 1000);
+    adc_signal_ = ""; // FIXME config_->get("adc_signal", "");
+    adc_freq_ = 1000; // FIXME config_->get("adc_frequency", 1000);
 
     if(!adc_signal_.empty()) {
         // Try it out directly to catch misconfiugration
         auto adc_value = device_->getADC(adc_signal_);
-        LOG(logger_, INFO) << "Will probe ADC signal \"" << adc_signal_ << "\" every " << std::to_string(adc_freq_)
-                           << " events";
+        LOG(logger_, INFO) << "Will probe ADC signal \"" << adc_signal_ << "\" every " << adc_freq_ << " frames";
+        LOG(logger_, TRACE) << "ADC value: " << adc_value; // FIXME: unused variable, send as stats instead
     }
 
-    LOG(logger_, STATUS) << "CaribouProducer configured. Ready to start run.";
+    LOG(logger_, STATUS) << getCanonicalName() << " launched";
 }
 
-void CaribouSatellite::landing(const std::stop_token& stop_token) {
+void CaribouSatellite::landing(const std::stop_token& /*stop_token*/) {
+    LOG(logger_, INFO) << "Switching off power for device " << device_->getName();
 
+    // Switch off the device power
     std::lock_guard<std::mutex> lock {device_mutex_};
-    LOG(logger_, INFO) << "Configuring device " << device_->getName();
-
-    // Switch off the device power:
     device_->powerOff();
+
+    LOG(logger_, STATUS) << getCanonicalName() << " landed";
 }
 
-void CaribouSatellite::reconfiguring(const std::stop_token& stop_token, const std::any& partial_config) {}
+void CaribouSatellite::reconfiguring(const std::stop_token& /*stop_token*/, const std::any& /*partial_config*/) {}
 
-void CaribouSatellite::starting(const std::stop_token& stop_token, std::uint32_t run_number) {
-    m_ev = 0;
+void CaribouSatellite::starting(const std::stop_token& /*stop_token*/, std::uint32_t run_number) {
+    LOG(logger_, INFO) << "Starting run " << run_number << "...";
 
-    LOG(logger_, INFO) << "Starting run...";
+    // Reset frame number
+    frame_nr_ = 0;
 
     // Start the DAQ
     std::lock_guard<std::mutex> lock {device_mutex_};
 
     // How to add additional information for the Begin-of-run event, e.g. containing tags with detector information?
-    // event->SetTag("software", device_->getVersion());
-    // event->SetTag("firmware", device_->getFirmwareVersion());
-    // event->SetTag("timestamp", LOGTIME);
-    // auto registers = device_->getRegisters();
-    // for(const auto& reg : registers) {
-    // event->SetTag(reg.first, reg.second);
-    // }
+    /*DataSender::BORMessage bor_msg {};
+    bor_msg.set_config(config_);
+    bor_msg.add_tag("software", device_->getVersion());
+    bor_msg.add_tag("firmware", device_->getFirmwareVersion());
+    const auto registers = device_->getRegisters();
+    for(const auto& reg : registers) {
+        bor_msg.add_tag(reg.first, reg.second);
+    }
+    data_sender_.sendBOR(std::move(bor_msg));*/
 
     // Start DAQ:
     device_->daqStart();
 
-    LOG(logger_, INFO) << "Started run.";
+    LOG(logger_, STATUS) << getCanonicalName() << " started (run " << run_number << ")";
 }
 
-void CaribouSatellite::stopping(const std::stop_token& stop_token) {
-
+void CaribouSatellite::stopping(const std::stop_token& /*stop_token*/) {
     LOG(logger_, INFO) << "Stopping run...";
 
     // Stop the DAQ
     std::lock_guard<std::mutex> lock {device_mutex_};
     device_->daqStop();
-    LOG(logger_, INFO) << "Stopped run.";
+
+    LOG(logger_, STATUS) << getCanonicalName() << " stopped";
 }
 
 void CaribouSatellite::running(const std::stop_token& stop_token) {
-
     LOG(logger_, INFO) << "Starting run loop...";
+
     std::lock_guard<std::mutex> lock {device_mutex_};
 
     while(!stop_token.stop_requested()) {
         try {
-            // Retrieve data from the device:
-            auto data = device_->getRawData();
+            // Retrieve data from the device
+            LOG(logger_, TRACE) << "Trying to receive data from device";
+            const auto data = device_->getRawData();
+
+            LOG(logger_, DEBUG) << "Frame " << frame_nr_;
 
             if(!data.empty()) {
-                // Create new event
-                auto event = Event::MakeUnique("Caribou" + name_ + "Event");
-                // Set event ID
-                event->SetEventN(m_ev);
-                // Add data to the event
-                event->AddBlock(0, data);
+                // Create new data message for data sender
+                /*DataSender::Message msg {};
+
+                // Set frame number
+                msg.addTag("frame", frame_nr_);
+
+                // Add data
+                msg.addDataFrame(std::move(data));
 
                 // Query ADC if wanted:
-                if(m_ev % adc_freq_ == 0) {
+                if(frame_nr_ % adc_freq_ == 0) {
                     if(!adc_signal_.empty()) {
                         auto adc_value = device_->getADC(adc_signal_);
                         LOG(logger_, DEBUG) << "ADC reading: " << adc_signal_ << " =  " << std::to_string(adc_value);
-                        event->SetTag(adc_signal_, adc_value);
+                        msg.addTag(adc_signal_, adc_value);
                     }
                 }
 
-                // We do not want to generate sub-events - send the event directly off to the Data Collector
-                SendEvent(std::move(event));
+                // Send data (async)
+                data_sender_.sendData(std::move(msg));*/
             }
 
-            // Now increment the event number
-            m_ev++;
-
-            LOG(logger_, DEBUG) << "Frame " << m_ev;
+            // Now increment the frame number
+            frame_nr_++;
         } catch(caribou::NoDataAvailable&) {
             continue;
         } catch(caribou::DataException& e) {
@@ -211,9 +241,9 @@ void CaribouSatellite::running(const std::stop_token& stop_token) {
         } catch(caribou::caribouException& e) {
             // FIXME sat exceptions missing.
             LOG(logger_, CRITICAL) << e.what();
-            throw;
+            throw std::exception();
         }
     }
 
-    LOG(logger_, INFO) << "Exiting run loop.";
+    LOG(logger_, INFO) << "Exiting run loop";
 }
