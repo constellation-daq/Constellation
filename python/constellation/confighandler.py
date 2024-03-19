55a3e87eda6528d4f4790a3be1d74d0c4e0464bd
Joel Ekström <joel.ekstrom@dvel.se>
2024-03-19 10:50:15 +0100

    docs: add doc-strings in Configuration

--- python/constellation/confighandler.py
+++ python/constellation/confighandler.py
@@ -13,27 +13,41 @@ class ConfigError(Exception):
 
 
 class Configuration:
+    """Class to track configuration variables and requests."""
+
     def __init__(self, config: dict = {}):
+        """Initialize configuration variables"""
         if not isinstance(config, dict):
             raise ConfigError
         self._config = config
         self._requested_keys: set[str] = set()
 
     def has_unused_values(self):
+        """Check if any unused configuration keys exist."""
         return not self._requested_keys == set(self._config.keys())
 
     def get_unused_keys(self):
+        """Return all unused configuration keys"""
         return set(self._config.keys()).difference(self._requested_keys)
 
     def get(self, key: str, default: any = None):
+        """
+        Return value from requested key in config with default value if specified.
+        Mark key as requested in configuration.
+        """
         self._requested_keys.add(key)
         return self._config.get(key, default)
 
     def __getitem__(self, key: str):
+        """
+        Return value from requested key in config.
+        Mark key as requested in configuration.
+        """
         self._requested_keys.add(key)
         return self._config[key]
 
     def get_keys(self):
+        """Return list of keys in config."""
         return list(self._config.keys())
 
 
