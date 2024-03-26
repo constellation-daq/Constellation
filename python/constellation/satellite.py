5bc11a9e54526c67d539824c033974c10847fa67
Joel Ekström <joel.ekstrom@dvel.se>
2024-03-26 11:31:49 +0100

    fix: add missing cscp_requestable decorator.

--- python/constellation/satellite.py
+++ python/constellation/satellite.py
@@ -373,6 +373,7 @@ class Satellite(CommandReceiver, CHIRPBroadcaster, SatelliteStateHandler):
         """
         return __version__, None, None
 
+    @cscp_requestable
     def get_state(self, _request: CSCPMessage = None) -> str:
         return self.fsm.current_state.id, None, None
 
