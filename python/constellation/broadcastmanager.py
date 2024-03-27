1855e46cb37b3f65f6fb77023b5c9f86a11cae5b
Hanno Perrey <hanno.perrey@dvel.se>
2024-03-27 09:23:51 +0100

    remove global COMMANDS and CALLBACKS
    use decorators to mark functions and generate list/dict at runtime

--- python/constellation/broadcastmanager.py
+++ python/constellation/broadcastmanager.py
@@ -24,21 +24,31 @@ from .chirp import (
 )
 
 
-CALLBACKS = dict()
-
-
 def chirp_callback(request_service: CHIRPServiceIdentifier):
-    """Register a function as a callback for CHIRP service requests."""
+    """Mark a function as a callback for CHIRP service requests."""
 
     def decorator(func):
         @wraps(func)
         def wrapper(*args, **kwargs):
             return func(*args, **kwargs)
 
-        CALLBACKS[request_service] = func
+        # mark function as chirp callback
+        wrapper.chirp_callback = request_service
         return wrapper
 
     return decorator
+
+
+def get_chirp_callbacks(cls):
+    """Loop over all class methods and return those marked as CHIRP callback."""
+    res = {}
+    for func in dir(cls):
+        call = getattr(cls, func)
+        if callable(call) and not func.startswith("__"):
+            # regular method
+            if hasattr(call, "chirp_callback"):
+                res[getattr(call, "chirp_callback")] = call
+    return res
 
 
 class DiscoveredService:
@@ -109,6 +119,7 @@ class CHIRPBroadcaster(BaseSatelliteFrame):
         self._registered_services = {}
         self.discovered_services = []
         self._chirp_thread = None
+        self._chirp_callbacks = get_chirp_callbacks(self)
 
         # set up logging
         self._logger = logging.getLogger(name + ".broadcast")
@@ -135,9 +146,9 @@ class CHIRPBroadcaster(BaseSatelliteFrame):
         self, serviceid: CHIRPServiceIdentifier, callback: callable
     ) -> None:
         """Register new callback for ServiceIdentifier."""
-        if serviceid in CALLBACKS:
+        if serviceid in self._chirp_callbacks:
             self._logger.info("Overwriting callback")
-        CALLBACKS[serviceid] = callback
+        self._chirp_callbacks[serviceid] = callback
         # make a callback if a service has already been discovered
         for known in self.get_discovered(serviceid):
             self.task_queue.put((callback, known))
@@ -155,7 +166,7 @@ class CHIRPBroadcaster(BaseSatelliteFrame):
         any incoming OFFERS will go unnoticed.
 
         """
-        if serviceid not in CALLBACKS:
+        if serviceid not in self._chirp_callbacks:
             self._logger.warning(
                 "Serviceid %s does not have a registered callback", serviceid
             )
@@ -174,7 +185,7 @@ class CHIRPBroadcaster(BaseSatelliteFrame):
 
     def broadcast_requests(self) -> None:
         """Broadcast all requests registered via register_request()."""
-        for serviceid in CALLBACKS:
+        for serviceid in self._chirp_callbacks:
             self._logger.debug("Broadcasting service REQUEST for %s", serviceid)
             self._beacon.broadcast(serviceid, CHIRPMessageType.REQUEST)
 
@@ -206,7 +217,7 @@ class CHIRPBroadcaster(BaseSatelliteFrame):
                 msg.from_address,
             )
             try:
-                callback = CALLBACKS[msg.serviceid]
+                callback = self._chirp_callbacks[msg.serviceid]
                 self.task_queue.put((callback, [self, service]))
             except KeyError:
                 self._logger.debug("No callback for service %s set up.", msg.serviceid)
@@ -227,7 +238,7 @@ class CHIRPBroadcaster(BaseSatelliteFrame):
             # indicate that service is no longer with us
             service.alive = False
             try:
-                callback = CALLBACKS[msg.serviceid]
+                callback = self._chirp_callbacks[msg.serviceid]
                 self.task_queue.put((callback, service))
             except KeyError:
                 self._logger.debug("No callback for service %s set up.", msg.serviceid)
--- python/tests/test_broadcastmanager.py
+++ python/tests/test_broadcastmanager.py
@@ -8,7 +8,7 @@ import pytest
 import time
 from unittest.mock import patch, MagicMock
 
-from constellation.broadcastmanager import CHIRPBroadcaster, chirp_callback, CALLBACKS
+from constellation.broadcastmanager import CHIRPBroadcaster, chirp_callback
 
 from constellation.chirp import CHIRPServiceIdentifier, CHIRP_PORT
 
@@ -132,7 +132,7 @@ def test_manager_callback_decorator(mock_bm_parent):
     """Test callback when discovering services registered via decorator."""
     # create callback
     assert mock_bm_parent.task_queue.empty()
-    assert len(CALLBACKS) == 1
+    assert len(mock_bm_parent._chirp_callbacks) == 1
     mock_packet_queue.append(offer_data_666)
     # thread running in background listening to "socket"
     time.sleep(0.5)
