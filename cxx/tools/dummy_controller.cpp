c5f7a38255abdb5fecf56859c50b9fd18b57f7d2
Stephan Lachnit <stephanlachnit@debian.org>
2024-04-08 19:16:52 +0200

    cxx: make dummy controller send proper payload and print received payload

--- cxx/tools/dummy_controller.cpp
+++ cxx/tools/dummy_controller.cpp
@@ -7,17 +7,22 @@
  * SPDX-License-Identifier: EUPL-1.2
  */
 
+#include <cstdint>
 #include <iostream>
 #include <memory>
 #include <string>
 
+#include <msgpack.hpp>
 #include <zmq.hpp>
 #include <zmq_addon.hpp>
 
+#include "constellation/core/config/Configuration.hpp"
+#include "constellation/core/config/Dictionary.hpp"
 #include "constellation/core/message/CSCP1Message.hpp"
 #include "constellation/core/message/satellite_definitions.hpp"
 #include "constellation/core/utils/casts.hpp"
 
+using namespace constellation::config;
 using namespace constellation::message;
 using namespace constellation::utils;
 
@@ -40,10 +45,15 @@ int main(int argc, char* argv[]) {
 
         // Send command
         auto send_msg = CSCP1Message({"dummy_controller"}, {CSCP1Message::Type::REQUEST, command});
-        if(command == "initialize" || command == "reconfigure" || command == "start") {
-            // HACK: add payload if initialize, reconfigure, or start
-            send_msg.addPayload(std::make_shared<zmq::message_t>("this is a dummy payload"));
-            std::cout << "added payload " << send_msg.hasPayload() << std::endl;
+        if(command == "initialize" || command == "reconfigure") {
+            send_msg.addPayload(Configuration().assemble());
+            std::cout << "Added empty configuration to message" << std::endl;
+        } else if(command == "start") {
+            const std::uint32_t run_nr = 1234U;
+            msgpack::sbuffer sbuf {};
+            msgpack::pack(sbuf, run_nr);
+            send_msg.addPayload(std::make_shared<zmq::message_t>(sbuf.data(), sbuf.size()));
+            std::cout << "Added run number " << run_nr << " to message" << std::endl;
         }
         send_msg.assemble().send(req);
 
@@ -55,5 +65,22 @@ int main(int argc, char* argv[]) {
         // Print message
         std::cout << recv_msg.getHeader().to_string() << "\n"
                   << "Verb: " << to_string(recv_msg.getVerb().first) << " : " << recv_msg.getVerb().second << std::endl;
+
+        // Print payload if dict
+        if(recv_msg.hasPayload()) {
+            try {
+                const auto dict = msgpack::unpack(to_char_ptr(recv_msg.getPayload()->data()), recv_msg.getPayload()->size())
+                                      ->as<Dictionary>();
+                if(!dict.empty()) {
+                    std::cout << "Payload:";
+                    for(const auto& [key, value] : dict) {
+                        std::cout << "\n " << key << ": " << value.str();
+                    }
+                    std::cout << std::endl;
+                }
+            } catch(...) {
+                std::cout << "Payload: <could not unpack payload>" << std::endl;
+            }
+        }
     }
 }
