11663aa473126885c25992fc242a48d73ca535ce
Simon Spannagel <simon.spannagel@desy.de>
2024-04-10 15:12:52 +0200

    cxx/core/metrics: separate files of metric class & manager

--- cxx/constellation/core/metrics/Manager.hpp
+++ cxx/constellation/core/metrics/Manager.hpp
@@ -18,81 +18,12 @@
 #include "constellation/core/config.hpp"
 #include "constellation/core/config/Dictionary.hpp"
 #include "constellation/core/logging/Logger.hpp"
+#include "constellation/core/metrics/Metric.hpp"
 
 namespace constellation::metrics {
 
-    /** Metrics types */
-    enum class Type : std::uint8_t {
-        LAST_VALUE = 1,
-        ACCUMULATE = 2,
-        AVERAGE = 3,
-        RATE = 4,
-    };
-    using enum Type;
-
-    using Clock = std::chrono::high_resolution_clock;
-
     /** Manager for Metrics handling & transmission */
     class CNSTLN_API Manager {
-    private:
-        class Metric {
-        public:
-            Metric(const Type type) : type_(type) {};
-
-            Metric(const Type type, config::Value value) : type_(type), value_(std::move(value)), changed_(true) {}
-
-            virtual ~Metric() noexcept = default;
-
-            // Default copy/move constructor/assignment
-            Metric(const Metric&) = default;
-            Metric& operator=(const Metric&) = default;
-            Metric(Metric&&) noexcept = default;
-            Metric& operator=(Metric&&) = default;
-
-            virtual void set(const config::Value& value);
-
-            config::Value value() const { return value_; }
-
-            Type type() const { return type_; }
-
-            bool check();
-            virtual Clock::time_point next_trigger() const { return Clock::time_point::max(); }
-
-        protected:
-            virtual bool condition() = 0;
-
-        private:
-            Type type_;
-            config::Value value_ {};
-            bool changed_ {false};
-        };
-
-        class TimedMetric : public Metric {
-        public:
-            TimedMetric(Clock::duration interval, Type type, config::Value value = {})
-                : Metric(type, std::move(value)), interval_(interval), last_trigger_(Clock::now()) {}
-
-            bool condition() override;
-            Clock::time_point next_trigger() const override;
-
-        private:
-            Clock::duration interval_;
-            Clock::time_point last_trigger_;
-        };
-
-        class TriggeredMetric : public Metric {
-        public:
-            TriggeredMetric(std::size_t triggers, Type type, config::Value value);
-
-            void set(const config::Value& value) override;
-
-            bool condition() override;
-
-        private:
-            std::size_t triggers_;
-            std::size_t current_triggers_ {0};
-        };
-
     public:
         /**
          * Return the default CHIRP Manager (requires to be set via `setAsDefaultInstance`)
