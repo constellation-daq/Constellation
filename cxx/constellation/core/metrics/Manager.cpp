3f69cfb88c6baec305078b8682e880847629dc7f
Simon Spannagel <simon.spannagel@desy.de>
2024-04-10 15:12:52 +0200

    cxx/core/metrics: separate files of metric class & manager

--- cxx/constellation/core/metrics/Manager.cpp
+++ cxx/constellation/core/metrics/Manager.cpp
@@ -20,62 +20,6 @@
 using namespace constellation::message;
 using namespace constellation::metrics;
 
-void Manager::Metric::set(const config::Value& value) {
-    if(value != value_) {
-        value_ = value;
-        changed_ = true;
-    }
-}
-
-bool Manager::Metric::check() {
-    if(!changed_) {
-        return false;
-    }
-    if(condition()) {
-        changed_ = false;
-        return true;
-    }
-    return false;
-}
-
-bool Manager::TimedMetric::condition() {
-
-    auto duration = Clock::now() - last_trigger_;
-
-    if(duration >= interval_) {
-        last_trigger_ += interval_;
-        return true;
-    }
-
-    return false;
-}
-
-Clock::time_point Manager::TimedMetric::next_trigger() const {
-    return last_trigger_ + interval_;
-}
-Manager::TriggeredMetric::TriggeredMetric(std::size_t triggers, Type type, config::Value value)
-    : Metric(type, value), triggers_(triggers) {
-    // We have an initial value, let's log it directly
-    if(!std::holds_alternative<std::monostate>(value)) {
-        current_triggers_ = triggers_;
-    }
-}
-
-void Manager::TriggeredMetric::set(const config::Value& value) {
-    Metric::set(value);
-    current_triggers_++;
-}
-
-bool Manager::TriggeredMetric::condition() {
-
-    if(current_triggers_ >= triggers_) {
-        current_triggers_ = 0;
-        return true;
-    }
-
-    return false;
-}
-
 Manager* Manager::getDefaultInstance() {
     return Manager::default_manager_instance_;
 }
