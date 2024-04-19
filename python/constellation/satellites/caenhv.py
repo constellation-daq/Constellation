95e7bfaa9b2d9911e5c4bc50ce1eb4c7dd9b422d
Hanno Perrey <hanno.perrey@dvel.se>
2024-04-19 10:46:38 +0200

    caen: implement state transitions and metrics

--- python/constellation/satellites/caenhv.py
+++ python/constellation/satellites/caenhv.py
@@ -8,13 +8,20 @@ This module provides the class for a Constellation Satellite.
 import logging
 import argparse
 import coloredlogs
+from functools import partial
 
 from ..core.satellite import Satellite
+from ..core.fsm import SatelliteState
 from ..core.commandmanager import cscp_requestable
 import pycaenhv
 
 
 class CaenHvSatellite(Satellite):
+    """Satellite controlling a CAEN HV crate via `pycaenhv`.
+
+    Supported models include SY5527.
+
+    """
 
     def do_initializing(self, configuration):
         """Set up connection to HV module and configure settings."""
@@ -69,24 +76,41 @@ class CaenHvSatellite(Satellite):
                                 f"Error in configuration for key {key}: {repr(e)}"
                             )
 
-        return "Connected and configured"
+        # configure metrics sending
+        self._configure_monitoring()
+        return f"Connected to crate and configured {len(crate.boards)} boards"
 
     def do_launching(self, payload):
-        # TODO implement
-        return "Launched"
+        """Power up the HV."""
+        nch = self._power_up()
+        return f"Launched and powered {nch} channels."
 
     def do_interrupting(self, payload):
-        # TODO implement
-        return "Interrupted"
+        """Power down but do not disconnect (e.g. keep monitoring)."""
+        self._power_down()
+        return "Interrupted and stopped HV."
 
     def fail_gracefully(self):
+        """Kill HV and disconnect."""
         if getattr(self, "caen", None):
+            self._power_down()
             self.caen.disconnect()
-        return "error handled"
+        return "Powered down and disconnected from crate."
 
     def get_channel_value(self, board: int, channel: int, par: str):
-        # TODO implement metric helper method
-        val = 42
+        """Return the value of a given channel parameter."""
+        if self.fsm.current_state in [
+            SatelliteState.NEW,
+            SatelliteState.ERROR,
+            SatelliteState.DEAD,
+        ]:
+            return None
+        try:
+            with self.caen as crate:
+                val = crate.boards[board].channels[channel].parameters[par]
+        except Exception as e:
+            val = None
+            self.log.exception(e)
         return val
 
     @cscp_requestable
@@ -104,9 +128,50 @@ class CaenHvSatellite(Satellite):
             val = crate.boards[board].channels[chno].parameters[par]
         return val, None, None
 
-    def about(self):
-        # TODO implement cmd
-        pass
+    @cscp_requestable
+    def about(self, _request):
+        """Get info about the Satellite"""
+        res = "{__name__} "
+        return res, None, None
+
+    def _power_down(self):
+        self.log.warning("Powering down all channels")
+        with self.caen as crate:
+            for brdno, brd in crate.boards.items():
+                for ch in brd.channels:
+                    ch.switch_off()
+        self.log.info("All channels powered down.")
+
+    def _configure_monitoring(self):
+        """Schedule monitoring for certain parameters."""
+        with self.caen as crate:
+            for brdno, brd in crate.boards.items():
+                # loop over boards
+                self.log.info("Configuring board %s", brd)
+                for chno, ch in enumerate(brd.channels):
+                    # loop over channels
+                    for par in ["IMon", "VMon"]:
+                        # add a callback using partial
+                        self.schedule_metric(
+                            f"b{brdno}_ch{chno}_{par}",
+                            partial(self.get_channel_value(brdno, chno, par)),
+                            10.0,
+                        )
+
+    def _power_up(self):
+        """Loop over channels and power them if they were configured such."""
+        npowered = 0  # number of powered channels
+        with self.caen as crate:
+            for brdno, brd in crate.boards.items():
+                self.log.info("Powering board %s", brd)
+                for chno, ch in enumerate(brd.channels):
+                    key = f"board{brdno}_ch{chno}_pw"
+                    self.log.debug("Powering board %s channel %s", brdno, chno)
+                    val = self.config.setdefault(key, "off")
+                    if val.lower() in ["true", "on", "1", "enabled", "enable"]:
+                        ch.switch_on()
+                        npowered += 1
+        return npowered
 
 
 # ---
