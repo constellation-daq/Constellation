ab2182a32ca288a638f22e7e3ed66b4bcba45e13
Simon Spannagel <simon.spannagel@cern.ch>
2024-05-02 07:20:35 +0200

    cxx/controller: use canonical name as key

--- cxx/constellation/controller/Controller.cpp
+++ cxx/constellation/controller/Controller.cpp
@@ -19,6 +19,7 @@
 #include "constellation/satellite/fsm_definitions.hpp"
 
 using namespace constellation::controller;
+using namespace constellation::message;
 using namespace constellation::utils;
 
 Controller::Controller(std::string_view controller_name) : logger_("CONTROLLER"), controller_name_(controller_name) {}
@@ -33,13 +34,26 @@ void Controller::registerSatellite(constellation::chirp::DiscoveredService servi
     // Add or drop, depending on message:
     const auto uri = "tcp://" + service.address.to_string() + ":" + std::to_string(service.port);
     if(depart) {
-        if(const auto it = satellite_connections_.find(service.host_id); it != satellite_connections_.end()) {
-            satellite_connections_.erase(it);
-            LOG(logger_, INFO) << "Satellite at " << uri << " departed";
+        for(auto it = satellite_connections_.begin(); it != satellite_connections_.end(); ++it) {
+            if(it->second.host_id == service.host_id) {
+                satellite_connections_.erase(it);
+                LOG(logger_, INFO) << "Satellite at " << uri << " departed";
+            }
         }
     } else {
-        Connection conn = {{context_, zmq::socket_type::req}, uri, ""};
-        const auto [it, success] = satellite_connections_.emplace(service.host_id, std::move(conn));
+        // New satellite connection
+        Connection conn = {{context_, zmq::socket_type::req}, service.host_id};
+        conn.req.connect(uri);
+
+        // Obtain canonical name:
+        auto send_msg = CSCP1Message({controller_name_}, {CSCP1Message::Type::REQUEST, "get_name"});
+        send_msg.assemble().send(conn.req);
+        zmq::multipart_t recv_zmq_msg {};
+        recv_zmq_msg.recv(conn.req);
+        const auto recv_msg = CSCP1Message::disassemble(recv_zmq_msg);
+        const auto name = recv_msg.getVerb().second;
+
+        const auto [it, success] = satellite_connections_.emplace(name, std::move(conn));
 
         if(!success) {
             LOG(logger_, DEBUG) << "Not adding remote satellite at " << uri << ", was already registered";
