8b3caf28ec146675b46ef744405965923f81359d
Simon Spannagel <simon.spannagel@cern.ch>
2024-05-08 21:49:54 +0200

    cxx/heartbeats: document manager

--- cxx/constellation/core/heartbeat/Manager.hpp
+++ cxx/constellation/core/heartbeat/Manager.hpp
@@ -27,13 +27,26 @@
 
 namespace constellation::heartbeat {
 
-    /** Manager for CHP publishing and receiving */
+    /**
+     * Manager for CHP publishing and receiving
+     *
+     * This manager holds a heartbeat sender and receiver as well as the logic for calling FSM interrupts based on
+     * received heartbeats. It keeps track of received heartbeats from remote heartbeat senders, counts their lives and
+     * takes action either upon missing heartbeats or a remote ERROR state of the FSM.
+     */
     class HeartbeatManager {
     public:
         /**
+         * Construct a heartbeat manager
+         *
+         * The constructor directly starts sender and receiver as well as the manager's watchdog thread which keeps track
+         * of remote heartbeat rates and states
+         *
+         * @param sender Canonical name of the heartbeat sender
          */
         CNSTLN_API HeartbeatManager(std::string_view sender);
 
+        /** Deconstruct the manager. This stops the watchdog thread */
         CNSTLN_API virtual ~HeartbeatManager();
 
         // No copy/move constructor/assignment
@@ -60,16 +73,41 @@ namespace constellation::heartbeat {
          */
         CNSTLN_API std::optional<message::State> getRemoteState(std::string_view remote);
 
+        /**
+         * @brief Set the interrupt callback
+         * @details This function allows setting the interrupt callback which is invoked when a remote heartbeat sender
+         * reports an ERROR state or stopped sending heartbeats
+         *
+         * @param fct [description]
+         */
         CNSTLN_API void setInterruptCallback(std::function<void()> fct) { interrupt_callback_ = std::move(fct); }
 
     private:
+        /**
+         * @brief Helper to process heartbeats. This is registered as callback in the heartbeat receiver
+         * @details It registers and updates the last heartbeat time point as well as the received state from remote
+         * heartbeat services
+         *
+         * @param msg Received CHP message from remote service
+         * */
         void process_heartbeat(const message::CHP1Message& msg);
 
+        /**
+         * @brief Main loop of the manager which checks for heartbeats of registered remotes.
+         * @details The thread sleeps until the next remote is expected to have sent a heartbeat, checks if any of the
+         * heartbeats are late or missing and goes back to sleep. This thread holds the main logic for autonomous operation,
+         * the reaction to remote ERROR states and the counting of lives as specified by the CHP protocol.
+         *
+         * @param stop_token Stop token to interrupt the thread
+         */
         void run(const std::stop_token& stop_token);
 
+        /** Receiver service */
         HeartbeatRecv receiver_;
+        /** Sender service */
         HeartbeatSend sender_;
 
+        /** Interrupt callback invoked upon remote failure condition and missing heartbeats */
         std::function<void()> interrupt_callback_;
 
         /**
@@ -84,6 +122,7 @@ namespace constellation::heartbeat {
             std::size_t lives {3};
         };
 
+        /** Map of remotes this manager tracks */
         std::map<std::string, Remote, std::less<>> remotes_;
         std::mutex mutex_;
         std::condition_variable cv_;
