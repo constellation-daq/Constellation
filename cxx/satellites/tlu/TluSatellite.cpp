f9e43c1063fc9c9bda18e638406482b22aa6541e
Finn Feindt <finn.feindt@desy.de>
2024-05-17 15:48:55 +0200

    TLU: drafty version of initialization procedure.

--- cxx/satellites/tlu/TluSatellite.cpp
+++ cxx/satellites/tlu/TluSatellite.cpp
@@ -26,22 +26,70 @@ TluSatellite::TluSatellite(std::string_view type, std::string_view name) : Satel
 void TluSatellite::initializing(constellation::config::Configuration& config) {
     LOG(logger_, INFO) << "Initializing " << getCanonicalName();
 
-    // Dummy use of configuration for something to supress warning
-    config.setDefault("banana", 1337);
-
-    // generate controler using hard coded paths
-    // ToDo: get from configuration
-
-    //auto ini = GetInitConfiguration();
-    //EUDAQ_INFO("TLU INITIALIZE ID: " + std::to_string(ini->Get("initid", 0)));
-    //std::string uhal_conn;
-    //std::string uhal_node;
-    //uhal_conn = ini->Get("ConnectionFile", uhal_conn);
-    //uhal_node = ini->Get("DeviceName",uhal_node);
-
-    std::string uhal_conn = "file://./../user/eudet/misc/hw_conf/aida_tlu/fmctlu_connection.xml";
+    LOG(logger_, INFO) << "TLU INITIALIZE ID: " + std::to_string(config.get<int>("initid", 0));
+    std::string uhal_conn = "file://./../../../cxx/satellites/tlu/default_config/aida_tlu_connection.xml";
     std::string uhal_node = "fmctlu.udp";
+    uhal_conn = config.get<std::string>("ConnectionFile", uhal_conn);
+    uhal_node = config.get<std::string>("DeviceName",uhal_node);
     m_tlu = std::unique_ptr<tlu::AidaTluController>(new tlu::AidaTluController(uhal_conn, uhal_node));
+
+    if( config.get<bool>("skipini", false) ){
+        LOG(logger_, INFO) << "TLU SKIPPING INITIALIZATION (skipini = 1)";
+    }
+    else{
+        m_verbose = config.get<uint8_t>("verbose",0);
+        LOG(logger_, INFO) << "TLU VERBOSITY SET TO: " + std::to_string(m_verbose);
+
+        // Define constants
+        m_tlu->DefineConst(config.get<int>("nDUTs", 4), config.get<int>("nTrgIn", 6));
+
+        // Import I2C addresses for hardware
+        // Populate address list for I2C elements
+        m_tlu->SetI2C_core_addr(config.get<uint8_t>("I2C_COREEXP_Addr", 0x21));
+        m_tlu->SetI2C_clockChip_addr(config.get<uint8_t>("I2C_CLK_Addr", 0x68));
+        m_tlu->SetI2C_DAC1_addr(config.get<uint8_t>("I2C_DAC1_Addr",0x13) );
+        m_tlu->SetI2C_DAC2_addr(config.get<uint8_t>("I2C_DAC2_Addr",0x1f) );
+        m_tlu->SetI2C_EEPROM_addr(config.get<uint8_t>("I2C_ID_Addr", 0x50) );
+        m_tlu->SetI2C_expander1_addr(config.get<uint8_t>("I2C_EXP1_Addr",0x74));
+        m_tlu->SetI2C_expander2_addr(config.get<uint8_t>("I2C_EXP2_Addr",0x75) );
+        m_tlu->SetI2C_pwrmdl_addr(config.get<uint8_t>("I2C_DACModule_Addr",  0x1C), config.get<uint8_t>("I2C_EXP1Module_Addr",  0x76), config.get<uint8_t>("I2C_EXP2Module_Addr",  0x77), config.get<uint8_t>("I2C_pwrId_Addr",  0x51));
+        m_tlu->SetI2C_disp_addr(config.get<uint8_t>("I2C_disp_Addr",0x3A));
+
+        // Initialize TLU hardware
+        m_tlu->InitializeI2C(m_verbose);
+        m_tlu->InitializeIOexp(m_verbose);
+        if (config.get<bool>("intRefOn", false)){
+            m_tlu->InitializeDAC(config.get<bool>("intRefOn", false), config.get<float>("VRefInt", 2.5), m_verbose);
+        }
+        else{
+            m_tlu->InitializeDAC(config.get<bool>("intRefOn", false), config.get<float>("VRefExt", 1.3), m_verbose);
+        }
+
+        // Initialize the Si5345 clock chip using pre-generated file
+        if (config.get<bool>("CONFCLOCK", true)){
+            std::string  clkConfFile;
+            std::string defaultCfgFile= "./../../../../cxx/satellites/tlu/default_config/aida_tlu_clk_config.txt";
+            clkConfFile= config.get<std::string>("CLOCK_CFG_FILE", defaultCfgFile);
+            if (clkConfFile== defaultCfgFile){
+                LOG(logger_, WARNING) << "TLU: Could not find the parameter for clock configuration in the INI file. Using the default.";
+            }
+            int clkres;
+            clkres= m_tlu->InitializeClkChip( clkConfFile, m_verbose  );
+            if (clkres == -1){
+                LOG(logger_, CRITICAL) << "TLU: clock configuration failed.";
+                // ToDo throw something at someone
+            }
+        }
+
+        // Reset IPBus registers
+        m_tlu->ResetSerdes();
+        m_tlu->ResetCounters();
+        m_tlu->SetTriggerVeto(1);
+        m_tlu->ResetFIFO();
+        m_tlu->ResetEventsBuffer();
+
+        m_tlu->ResetTimestamp();
+    }
 }
 
 void TluSatellite::launching() {
