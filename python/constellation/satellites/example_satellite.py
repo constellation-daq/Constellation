64bac1310d1569ed67c11fa0ac72dd6ae27c1c15
Håkan Wennlöf <h.wennlof@cern.ch>
2024-05-17 17:53:59 +0200

    Reverted earlier now-unused changes to meson files et al.

--- python/constellation/satellites/example_satellite.py
+++ python/constellation/satellites/example_satellite.py
@@ -6,37 +6,45 @@ SPDX-License-Identifier: CC-BY-4.0
 This module provides the class for a Constellation Satellite.
 """
 
-from constellation.core.satellite import Satellite
+from constellation.core.satellite import Satellite, SatelliteArgumentParser
 import time
 import logging
 from constellation.core.configuration import ConfigError, Configuration
+from constellation.core.base import EPILOG
+
+
+"""
+Mock class representing a device that can be utilised by a satellite
+"""
 
 
 class Example_Device1:
-
-    def set_config(self, config: Configuration):
-        self.voltage = config["voltage"]
-        self.ampere = config["ampere"]
-        self.sample_period = config.setdefault("sample_period", default=0.1)
+    def __init__(self, voltage, ampere, sample_period=0.1):
+        self.voltage = voltage
+        self.ampere = ampere
+        self.sample_period = sample_period
 
 
 class Example_Satellite(Satellite):
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        self.device = Example_Device1()
 
-    def do_initializing(self, payload: dict) -> str:
-        super().do_initializing(payload=payload)
+    def do_initializing(self, config: Configuration) -> str:
         try:
-            self.device.set_config(self.config)
+            self.device = Example_Device1(
+                config["voltage"], config["current"], config["sample_period"]
+            )
         except KeyError as e:
-            self.log.error("Configuration has no attribute %s", e)
+            self.log.error(
+                "Attribute '%s' is required but missing from the configuration.", e
+            )
             raise ConfigError
 
         return "Initialized"
 
     def do_run(self, payload: any) -> str:
         while not self._state_thread_evt.is_set():
+            """
+            Example work to be done while satellite is running
+            """
             time.sleep(self.device.sample_period)
             print(f"New sample at {self.device.voltage}")
         return "Finished acquisition."
@@ -46,34 +54,28 @@ class Example_Satellite(Satellite):
 
 
 def main(args=None):
-    """Start the base Satellite server."""
-    import argparse
+    """Start an example satellite.
+
+    Provides a basic example satellite that can be controlled, and used as a basis for implementations.
+    """
     import coloredlogs
 
-    parser = argparse.ArgumentParser(description=main.__doc__)
-    parser.add_argument("--log-level", default="info")
-    parser.add_argument("--cmd-port", type=int, default=23999)
-    parser.add_argument("--mon-port", type=int, default=55556)
-    parser.add_argument("--hb-port", type=int, default=61234)
-    parser.add_argument("--interface", type=str, default="*")
-    parser.add_argument("--name", type=str, default="satellite_demo")
-    parser.add_argument("--group", type=str, default="constellation")
-    args = parser.parse_args(args)
+    parser = SatelliteArgumentParser(description=main.__doc__, epilog=EPILOG)
+    # this sets the defaults for our "demo" Satellite
+    parser.set_defaults(
+        name="satellite_demo", cmd_port=23999, mon_port=55556, hb_port=61234
+    )
+    # get a dict of the parsed arguments
+    args = vars(parser.parse_args(args))
 
     # set up logging
-    logger = logging.getLogger(args.name)
-    coloredlogs.install(level=args.log_level.upper(), logger=logger)
+    logger = logging.getLogger(args["name"])
+    log_level = args.pop("log_level")
+    coloredlogs.install(level=log_level.upper(), logger=logger)
 
     logger.info("Starting up satellite!")
     # start server with remaining args
-    s = Satellite(
-        name=args.name,
-        group=args.group,
-        cmd_port=args.cmd_port,
-        hb_port=args.hb_port,
-        mon_port=args.mon_port,
-        interface=args.interface,
-    )
+    s = Example_Satellite(**args)
     s.run_satellite()
 
 
