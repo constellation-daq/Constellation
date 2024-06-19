0b0655c7bd0985e7761d0895ec7df1443e86a18b
Stephan Lachnit <stephan.lachnit@desy.de>
2024-06-19 11:45:09 +0200

    cxx/logging: only enable CMDP after name is set
    By default, the global CMDP log level is OFF, meaning no messages are forwarded to the CMDP sink. Only once the subscription thread starts, this log level will be adjusted. Thus the subscription thread is now started in the `enableSending` method of the sink, which also sets the name and registers the CHIRP service.
    
    Signed-off-by: Stephan Lachnit <stephanlachnit@debian.org>

--- cxx/constellation/core/logging/CMDPSink.hpp
+++ cxx/constellation/core/logging/CMDPSink.hpp
@@ -10,10 +10,13 @@
 #pragma once
 
 #include <map>
+#include <memory>
 #include <mutex>
+#include <stop_token>
 #include <string>
 #include <thread>
 
+#include <spdlog/async_logger.h>
 #include <spdlog/sinks/base_sink.h>
 #include <zmq.hpp>
 
@@ -30,8 +33,10 @@ namespace constellation::log {
     public:
         /**
          * Construct a new CMDPSink
+         *
+         * @param cmdp_console_logger Console logger for CMDP
          */
-        CMDPSink();
+        CMDPSink(std::shared_ptr<spdlog::async_logger> cmdp_console_logger);
 
         /**
          * Deconstruct the CMDPSink
@@ -52,27 +57,30 @@ namespace constellation::log {
         constexpr utils::Port getPort() const { return port_; }
 
         /**
-         * Set sender name
+         * Set sender name and enable sending by starting the subscription thread
          *
          * @param sender_name Canonical name of the sender
          */
-        void setSender(std::string sender_name);
+        void enableSending(std::string sender_name);
 
     protected:
         void sink_it_(const spdlog::details::log_msg& msg) final;
         void flush_() final {}
 
     private:
+        void subscription_loop(const std::stop_token& stop_token);
+
+    private:
+        std::shared_ptr<spdlog::async_logger> cmdp_console_logger_;
+
         zmq::context_t context_;
         zmq::socket_t publisher_;
 
-        std::jthread subscription_thread_;
-        void subscription_loop(const std::stop_token& stop_token);
-        std::map<std::string, std::map<Level, std::size_t>> log_subscriptions_;
-
         utils::Port port_;
         std::string sender_name_;
-        std::once_flag setup_flag_;
+
+        std::jthread subscription_thread_;
+        std::map<std::string, std::map<Level, std::size_t>> log_subscriptions_;
     };
 
 } // namespace constellation::log
