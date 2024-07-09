b08e5da0d3c41d9cec5f6eab42a65600a4b4ce82
Hanno Perrey <hanno.perrey@dvel.se>
2024-07-09 13:54:10 +0200

    python/tests: replace sleep with 'wait for' logic

--- python/tests/test_controller.py
+++ python/tests/test_controller.py
@@ -23,7 +23,10 @@ def test_controller_instantiation(mock_controller):
 @pytest.mark.forked
 def test_satellite_access_via_array(mock_controller, mock_satellite):
     """Test state cmd reception via satellite array."""
-    time.sleep(0.1)
+    timeout = 2
+    while timeout > 0 and len(mock_controller.constellation.satellites) < 1:
+        time.sleep(0.05)
+        timeout -= 0.05
     res = mock_controller.constellation.Satellite.mock_satellite.get_state()
     assert res["msg"] == "new"
 
@@ -31,7 +34,10 @@ def test_satellite_access_via_array(mock_controller, mock_satellite):
 @pytest.mark.forked
 def test_satellite_hb_state(mock_controller, mock_satellite):
     """Test heartbeat state check."""
-    time.sleep(0.1)
+    timeout = 2
+    while timeout > 0 and len(mock_controller.states) < 1:
+        time.sleep(0.05)
+        timeout -= 0.05
     assert mock_controller.states["Satellite.mock_satellite"].name.lower() == "new"
 
 
@@ -39,7 +45,10 @@ def test_satellite_hb_state(mock_controller, mock_satellite):
 def test_satellite_init_w_fullcfg(mock_controller, mock_example_satellite, rawconfig):
     """Test cmd reception."""
     satellite = mock_example_satellite
-    time.sleep(0.1)
+    timeout = 2
+    while timeout > 0 and len(mock_controller.constellation.satellites) < 1:
+        time.sleep(0.05)
+        timeout -= 0.05
     res = mock_controller.constellation.MockExampleSatellite.mock_satellite.initialize(
         rawconfig
     )
