ca961eef610c88eea53c4264aecbc8c7c3a63080
Simon Spannagel <simon.spannagel@cern.ch>
2024-07-21 22:09:55 +0200

    cxx/controller: first draft for measurement queue class

---
+++ cxx/constellation/controller/MeasurementQueue.cpp
@@ -0,0 +1,13 @@
+/**
+ * @file
+ * @brief Implementation of a measurement queue
+ *
+ * @copyright Copyright (c) 2024 DESY and the Constellation authors.
+ * This software is distributed under the terms of the EUPL-1.2 License, copied verbatim in the file "LICENSE.md".
+ * SPDX-License-Identifier: EUPL-1.2
+ */
+
+#include "MeasurementQueue.hpp"
+
+using namespace constellation::config;
+using namespace constellation::controller;
