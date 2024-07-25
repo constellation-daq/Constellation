83ecfedb522b92cde79c5d67886f96bdeee9b305
Hanno Perrey <hanno.perrey@dvel.se>
2024-07-25 15:31:18 +0200

    python/tests: fix controller tests

--- python/tests/test_controller.py
+++ python/tests/test_controller.py
@@ -28,7 +28,7 @@ def test_satellite_access_via_array(mock_controller, mock_satellite):
         time.sleep(0.05)
         timeout -= 0.05
     res = mock_controller.constellation.Satellite.mock_satellite.get_state()
-    assert res["msg"] == "new"
+    assert res.msg == "new"
 
 
 @pytest.mark.forked
@@ -52,7 +52,7 @@ def test_satellite_init_w_fullcfg(mock_controller, mock_example_satellite, rawco
     res = mock_controller.constellation.MockExampleSatellite.mock_satellite.initialize(
         rawconfig
     )
-    assert res["msg"] == "transitioning"
+    assert res.msg == "transitioning"
     time.sleep(0.1)
     assert satellite.config._config == flatten_config(
         rawconfig, "MockExampleSatellite", "mock_satellite"
