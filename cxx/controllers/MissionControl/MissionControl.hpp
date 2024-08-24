6810a1c220bb0153e76c9bb8120754a6a8507e4a
Simon Spannagel <simon.spannagel@cern.ch>
2024-08-25 00:20:28 +0200

    cxx/MissionControl: add reuse identififers

--- cxx/controllers/MissionControl/MissionControl.hpp
+++ cxx/controllers/MissionControl/MissionControl.hpp
@@ -1,3 +1,14 @@
+/**
+ * @file
+ * @brief MissionControl GUI implementation
+ *
+ * @copyright Copyright (c) 2024 DESY and the Constellation authors.
+ * This software is distributed under the terms of the EUPL-1.2 License, copied verbatim in the file "LICENSE.md".
+ * SPDX-License-Identifier: EUPL-1.2
+ */
+
+#pragma once
+
 #include <QCloseEvent>
 #include <QDir>
 #include <QFileDialog>
--- cxx/controllers/MissionControl/QController.hpp
+++ cxx/controllers/MissionControl/QController.hpp
@@ -1,3 +1,14 @@
+/**
+ * @file
+ * @brief QController definition
+ *
+ * @copyright Copyright (c) 2024 DESY and the Constellation authors.
+ * This software is distributed under the terms of the EUPL-1.2 License, copied verbatim in the file "LICENSE.md".
+ * SPDX-License-Identifier: EUPL-1.2
+ */
+
+#pragma once
+
 #include <array>
 #include <memory>
 #include <optional>
