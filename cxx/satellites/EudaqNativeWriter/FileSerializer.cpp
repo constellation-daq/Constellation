6cf8dae2badee4e8c4215058b8937247fa67982a
Simon Spannagel <simon.spannagel@cern.ch>
2024-09-05 08:33:27 +0200

    EudaqNativeWriter: add file header for reuse

--- cxx/satellites/EudaqNativeWriter/FileSerializer.cpp
+++ cxx/satellites/EudaqNativeWriter/FileSerializer.cpp
@@ -1,3 +1,12 @@
+/**
+ * @file
+ * @brief Implementation of EUDAQ data serializer
+ *
+ * @copyright Copyright (c) 2024 DESY and the Constellation authors.
+ * This software is distributed under the terms of the EUPL-1.2 License, copied verbatim in the file "LICENSE.md".
+ * SPDX-License-Identifier: EUPL-1.2
+ */
+
 #include "EudaqNativeWriterSatellite.hpp"
 
 using namespace constellation::config;
