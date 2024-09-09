3fee4e85872aab59e68ac38a44babf40d10651ab
Simon Spannagel <simon.spannagel@cern.ch>
2024-09-09 18:59:27 +0200

    cxx/sat/fsm: prepare for remote state conditions

--- cxx/constellation/satellite/FSM.cpp
+++ cxx/constellation/satellite/FSM.cpp
@@ -237,6 +237,29 @@ void FSM::terminate() {
     join_failure_thread();
 }
 
+void FSM::registerRemoteCallback(std::function<std::optional<State>(std::string_view)> callback) {
+    remote_callback_ = std::move(callback);
+}
+
+void FSM::registerRemoteCondition(std::string remote, State transitional) {
+
+    // Check that the requested remote is not this satellite:
+    if(utils::transform(remote, ::tolower) == utils::transform(satellite_->getCanonicalName(), ::tolower)) {
+        throw std::invalid_argument("Conditions cannot be applied to same satellite");
+    }
+
+    // Check that the state is not a steady state but a transitional state:
+    if(is_steady(transitional)) {
+        throw std::invalid_argument("Conditions cannot be applied to steady state " + to_string(transitional));
+    }
+
+    remote_conditions_.emplace(remote, transitional);
+}
+
+void FSM::clearRemoteCondition() {
+    remote_conditions_.clear();
+}
+
 void FSM::call_state_callbacks(bool only_with_status) {
     const std::lock_guard state_callbacks_lock {state_callbacks_mutex_};
 
