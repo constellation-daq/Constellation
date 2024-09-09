548286ebfb405f4ed1e67d8b146d7be022f7fa84
Simon Spannagel <simon.spannagel@cern.ch>
2024-09-09 19:48:39 +0200

    cxx/satellite/fsm: add conditional waiting logic

--- cxx/constellation/satellite/FSM.cpp
+++ cxx/constellation/satellite/FSM.cpp
@@ -35,6 +35,7 @@
 #include "constellation/core/utils/exceptions.hpp"
 #include "constellation/core/utils/msgpack.hpp"
 #include "constellation/core/utils/string.hpp"
+#include "constellation/core/utils/timers.hpp"
 #include "constellation/satellite/BaseSatellite.hpp"
 #include "constellation/satellite/exceptions.hpp"
 
@@ -264,6 +265,66 @@ void FSM::join_failure_thread() {
 template <typename Func, typename... Args>
 FSM::Transition FSM::call_satellite_function(Func func, Transition success_transition, Args&&... args) {
     std::string error_message {};
+
+    // Check if transition conditions are satisfied:
+    if(remote_callback_ && !remote_conditions_.empty()) {
+
+        const TimeoutTimer timer {remote_condition_timeout_};
+        while(true) {
+            bool satisfied = true;
+            for(const auto& condition : remote_conditions_) {
+                // Check if this condition applies to current state:
+                if(condition.applies(state_.load())) {
+                    // Get remote state:
+                    auto remote_state = remote_callback_(condition.remote);
+
+                    // Fail if the satellite to which this condition applies is not present in the constellation
+                    if(!remote_state.has_value()) {
+                        error_message = "Dependent remote satellite " + condition.remote + " not present";
+                        LOG(logger_, CRITICAL) << "Critical failure during transition: " << error_message;
+                        satellite_->set_status("Critical failure during transition: " + error_message);
+                        return Transition::failure;
+                    }
+
+                    // Check if state is ERROR:
+                    if(remote_state.value() == State::ERROR) {
+                        error_message = "Dependent remote satellite " + condition.remote + " reports state " +
+                                        to_string(remote_state.value());
+                        LOG(logger_, CRITICAL) << "Critical failure during transition: " << error_message;
+                        satellite_->set_status("Critical failure during transition: " + error_message);
+                        return Transition::failure;
+                    }
+
+                    // Check if condition is fulfilled:
+                    if(!condition.is_satisfied(remote_state.value())) {
+                        LOG(logger_, STATUS) << "Awaiting state from " << condition.remote
+                                             << ", currently: " << to_string(remote_state.value());
+                        satisfied = false;
+                        break;
+                    }
+                }
+            }
+
+            // If all conditions are satisfied, continue:
+            if(satisfied) {
+                LOG(logger_, DEBUG) << "Satisfied with all remote conditions, continuing";
+                break;
+            }
+
+            // If timeout reached, throw
+            if(timer.timeoutReached()) {
+                error_message = "Could not satisfy remote conditions within timeout";
+                LOG(logger_, CRITICAL) << "Critical failure during transition: " << error_message;
+                satellite_->set_status("Critical failure during transition: " + error_message);
+                return Transition::failure;
+            }
+
+            // Wait a bit before checking again
+            using namespace std::literals::chrono_literals;
+            std::this_thread::sleep_for(100ms);
+        }
+    }
+
     try {
         // Call transition function of satellite
         (satellite_->*func)(std::forward<Args>(args)...);
