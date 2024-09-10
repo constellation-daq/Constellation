47834069d4edea0c9e4c5bd76ead37bce9774356
Simon Spannagel <simon.spannagel@desy.de>
2024-09-10 14:20:23 +0200

    Katherine: add two separate mutexes for acquisition variable access & CTRL UDP socket

--- cxx/satellites/Katherine/KatherineSatellite.hpp
+++ cxx/satellites/Katherine/KatherineSatellite.hpp
@@ -110,7 +110,8 @@ private:
 
 private:
     std::shared_ptr<katherine::device> device_;
-    std::mutex device_mutex_;
+    std::mutex katherine_cmd_mutex_;
+    std::mutex katherine_data_mutex_;
 
     std::shared_ptr<katherine::base_acquisition> acquisition_;
     katherine::config katherine_config_ {};
