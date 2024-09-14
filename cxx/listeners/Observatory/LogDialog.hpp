07d8c8ca03327f7f79c43555f9073cda581717e5
Simon Spannagel <simon.spannagel@cern.ch>
2024-09-14 13:25:59 +0200

    Observatory: add reuse

--- cxx/listeners/Observatory/LogDialog.hpp
+++ cxx/listeners/Observatory/LogDialog.hpp
@@ -1,5 +1,13 @@
-#ifndef INCLUDED_LogDialog_hh
-#define INCLUDED_LogDialog_hh
+/**
+ * @file
+ * @brief Log Dialog
+ *
+ * @copyright Copyright (c) 2024 DESY and the Constellation authors.
+ * This software is distributed under the terms of the EUPL-1.2 License, copied verbatim in the file "LICENSE.md".
+ * SPDX-License-Identifier: EUPL-1.2
+ */
+
+#pragma once
 
 #include "QLogListener.hpp"
 #include "ui_LogDialog.h"
@@ -16,5 +24,3 @@ public:
         show();
     }
 };
-
-#endif // INCLUDED_LogDialog_hh
