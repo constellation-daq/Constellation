40db7868975722604fa8e9898b2ee205b6f21aa0
Simon Spannagel <simon.spannagel@cern.ch>
2024-09-14 13:16:53 +0200

    Observatory: some code cleanup

--- cxx/listeners/Observatory/QLogListener.hpp
+++ cxx/listeners/Observatory/QLogListener.hpp
@@ -23,7 +23,6 @@ public:
     explicit LogMessage(constellation::message::CMDP1LogMessage msg);
 
     QString operator[](int) const;
-    std::string Text(int) const;
 
     static int NumColumns() { return headers_.size() - 1; }
     static int NumExtendedColumns() { return headers_.size(); }
@@ -53,12 +52,32 @@ class QLogListener : public QAbstractListModel,
     Q_OBJECT
 
 public:
-    QLogListener(QObject* parent = 0);
+    /**
+     * @brief Constructor of QLogListener
+     *
+     * @param parent QObject parent
+     */
+    explicit QLogListener(QObject* parent = 0);
 
-    constellation::log::Level GetLevel(const QModelIndex& index) const;
-    bool IsDisplayed(size_t index);
+    /**
+     * @brief Obtain the log level of a message at a given model index
+     *
+     * @param index QModelIndex of the message in question
+     * @return Log level of the message
+     */
+    constellation::log::Level getMessageLevel(const QModelIndex& index) const;
 
+    /**
+     * @brief Set a new log level filter value
+     *
+     * @param level New log level filter
+     */
     void setFilterLevel(constellation::log::Level level);
+
+    /**
+     * @brief Return the currently set log level filter
+     * @return Log level filter
+     */
     constellation::log::Level getFilterLevel() const { return filter_level_; }
 
     /**
@@ -68,6 +87,11 @@ public:
      * @return True if the filter was updated, false otherwise
      */
     bool setFilterSender(const std::string& sender);
+
+    /**
+     * @brief Return the currently set sender filter
+     * @return Sender filter
+     */
     std::string getFilterSender() const { return filter_sender_; }
 
     /**
@@ -77,6 +101,11 @@ public:
      * @return True if the filter was updated, false otherwise
      */
     bool setFilterTopic(const std::string& topic);
+
+    /**
+     * @brief Return the currently set topic filter
+     * @return Topic filter
+     */
     std::string getFilterTopic() const { return filter_topic_; }
 
     /**
@@ -85,14 +114,33 @@ public:
      * @param pattern Message filter pattern
      */
     void setFilterMessage(const QString& topic);
+
+    /**
+     * @brief Return the currently set message filter pattern
+     * @return Message filter pattern
+     */
     QString getFilterMessage() const { return filter_message_.pattern(); }
 
+    /**
+     * @brief Update the global subscription log level
+     * @param level New global subscription log level
+     */
     void setGlobalSubscriptionLevel(constellation::log::Level level);
+
+    /**
+     * @brief Get current global log subscription level
+     * @return Global log subscription level
+     */
     constellation::log::Level getGlobalSubscriptionLevel() const { return subscription_global_level_; }
 
-    void UpdateDisplayed();
+    /**
+     * @brief Obtain a message from a given QModelIndex
+     *
+     * @param index QModelIndex of the message
+     * @return Constant reference to the message
+     */
+    const LogMessage& getMessage(const QModelIndex& index) const;
 
-    const LogMessage& GetMessage(int row) const;
     int rowCount(const QModelIndex& parent = QModelIndex()) const override;
     int columnCount(const QModelIndex& parent = QModelIndex()) const override;
     QVariant data(const QModelIndex& index, int role) const override;
@@ -121,8 +169,36 @@ signals:
     void newTopic(QString topic);
 
 private:
+    /**
+     * @brief Callback registered for receiving log messages from the subscription pool
+     * This function inserts the message into storage, checks for new topics or sender names to register, and emits a
+     * newMessage signal with the inserted index when the message is displayed.
+     *
+     * \param msg Received log message
+     */
     void add_message(constellation::message::CMDP1LogMessage&& msg);
 
+    /**
+     * @brief Helper to determine if a message at the given index should be displayed currently.
+     *
+     * This evaluates the currently-configured filters.
+     *
+     * @param index Index of the message in the message storage
+     * @return True if the message is to be displayed, false otherwise
+     */
+    bool is_message_displayed(size_t index);
+
+    /**
+     * @brief Helper to update the list of displayed messages
+     */
+    void update_displayed_messages();
+
+    /**
+     * @brief Helper to get all subscription topics given a global subscription log level.This is used to immediately
+     * subscribe new sockets appearing to the global log level and all topics below
+
+     * @return Set of subscription topics
+     */
     std::set<std::string> get_global_subscription_topics();
 
 private:
@@ -130,18 +206,18 @@ private:
     constellation::log::Logger logger_;
 
     /** Subscription & storage */
-    std::vector<LogMessage> m_all;
+    std::vector<LogMessage> messages_;
     constellation::log::Level subscription_global_level_ {constellation::log::Level::INFO};
 
     /** Filter & display */
-    std::vector<size_t> m_disp;
+    std::vector<size_t> display_indices_;
     constellation::log::Level filter_level_ {constellation::log::Level::TRACE};
     std::set<std::string> filter_sender_list_ {"- All -"};
     std::string filter_sender_ {"- All -"};
     std::set<std::string> filter_topic_list_ {"- All -"};
     std::string filter_topic_ {"- All -"};
-
     QRegularExpression filter_message_;
 
+    /** Sorting helper */
     LogSorter m_sorter;
 };
