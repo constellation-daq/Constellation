8534dfad8ed48f0a366c21ab208decab0aafac8b
Simon Spannagel <simon.spannagel@desy.de>
2024-09-19 15:24:46 +0200

    CI: enable all controllers and listeners

---
+++ scripts/meson_enable_all_controllers.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+# SPDX-FileCopyrightText: 2024 DESY and the Constellation authors
+# SPDX-License-Identifier: CC0-1.0
+
+MESON_OPTIONS_TXT="$(realpath $(dirname $0)/..)/meson_options.txt"
+
+# Look for all options starting with controller_ and enable them
+grep "option('controller_" $MESON_OPTIONS_TXT | sed -E -e "s%option\('(controller_\w+)'.*%\1%g" | sed -E -e "s%(.*)%-D\1=enabled%g" | paste -sd " " - | cat
