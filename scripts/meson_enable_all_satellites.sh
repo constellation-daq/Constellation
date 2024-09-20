0f958b06cb74819b97f722e6249912e1334e387a
Simon Spannagel <simon.spannagel@desy.de>
2024-09-20 15:39:43 +0200

    Meson: merge scripts

--- scripts/meson_enable_all_satellites.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-# SPDX-FileCopyrightText: 2024 DESY and the Constellation authors
-# SPDX-License-Identifier: CC0-1.0
-
-MESON_OPTIONS_TXT="$(realpath $(dirname $0)/..)/meson_options.txt"
-
-# Look for all options starting with satellite_ and enable them
-grep "option('satellite_" $MESON_OPTIONS_TXT | sed -E -e "s%option\('(satellite_\w+)'.*%\1%g" | sed -E -e "s%(.*)%-D\1=enabled%g" | paste -sd " " - | cat
