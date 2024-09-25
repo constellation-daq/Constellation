87db63a971d5940dd45acbb6376a4bb4c7d2a737
Stephan Lachnit <stephanlachnit@debian.org>
2024-09-25 17:10:13 +0200

    caenhv: format and disable do_reconfigure
    Simply calling initialize again is not sufficient for online reconfiguration. Then one might as well use land+initialize+launch again.

--- python/constellation/satellites/caenhv/caenhv.py
+++ python/constellation/satellites/caenhv/caenhv.py
@@ -56,9 +56,7 @@ class CaenHvSatellite(Satellite):
             user,
             pw,
         )
-        self.caen.connect(
-            system=system, link=link, argument=link_arg, user=user, password=pw
-        )
+        self.caen.connect(system=system, link=link, argument=link_arg, user=user, password=pw)
         if not self.caen.is_connected():
             raise RuntimeError("No connection to Caen HV crate established")
 
@@ -89,9 +87,7 @@ class CaenHvSatellite(Satellite):
                             # nothing in the cfg, leave as it is
                             continue
                         except ValueError as e:
-                            raise RuntimeError(
-                                f"Error in configuration for key {key}: {repr(e)}"
-                            ) from e
+                            raise RuntimeError(f"Error in configuration for key {key}: {repr(e)}") from e
                         ch.parameters[par].value = val
                         self.log.debug(
                             "Configuring %s on board %s, ch %s with value '%s'",
@@ -105,10 +101,11 @@ class CaenHvSatellite(Satellite):
         self._configure_monitoring()
         return f"Connected to crate and configured {len(crate.boards)} boards"
 
-    def do_reconfigure(self, configuration: Configuration) -> str:
-        """Reconfigure the HV module by re-running initialization and launch."""
-        self.do_initializing(configuration)
-        return self.do_launching(None)
+    # def do_reconfigure(self, partial_config: Configuration) -> str:
+    #    """Reconfigure the HV module by re-running initialization and launch."""
+    #    FIXME: needs updating! See https://gitlab.desy.de/constellation/constellation/-/issues/153
+    #    self.do_initializing(configuration)
+    #    return self.do_launching(None)
 
     def do_launching(self, payload: Any) -> str:
         """Power up the HV."""
@@ -206,9 +203,7 @@ class CaenHvSatellite(Satellite):
 
         """
         if not self._ready():
-            raise RuntimeError(
-                f"Command not allowed in state '{self.fsm.current_state_value.name}'"
-            )
+            raise RuntimeError(f"Command not allowed in state '{self.fsm.current_state_value.name}'")
         res = {}
         with self.caen as crate:
             for brdno, brd in crate.boards.items():
@@ -284,9 +279,7 @@ class CaenHvSatellite(Satellite):
                         npowered += 1
                     else:
                         if ch.is_powered():
-                            self.log.info(
-                                "Powering DOWN board %s channel %s", brdno, chno
-                            )
+                            self.log.info("Powering DOWN board %s channel %s", brdno, chno)
                             ch.switch_off()
         return npowered
 
