1e78e510f6a34472c63df3f2dacb4aba4c65b8a0
Simon Spannagel <simon.spannagel@cern.ch>
2024-10-02 08:53:37 +0200

    Observatory: code cleanup

--- cxx/listeners/Observatory/QLogListener.hpp
+++ cxx/listeners/Observatory/QLogListener.hpp
@@ -132,10 +132,10 @@ public:
     /// @cond doxygen_suppress
 
     /* Qt accessor methods */
-    int rowCount(const QModelIndex& parent = QModelIndex()) const override;
-    int columnCount(const QModelIndex& parent = QModelIndex()) const override;
+    int rowCount(const QModelIndex& parent) const override;
+    int columnCount(const QModelIndex& parent) const override;
     QVariant data(const QModelIndex& index, int role) const override;
-    QVariant headerData(int section, Qt::Orientation orientation, int role = Qt::DisplayRole) const override;
+    QVariant headerData(int column, Qt::Orientation orientation, int role = Qt::DisplayRole) const override;
 
     /// @endcond
 
