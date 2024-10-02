9b8b1135a1dc657119799cae5a81b31b75e8fd5b
Simon Spannagel <simon.spannagel@desy.de>
2024-10-02 11:28:04 +0200

    Observatory: document remaining files

--- cxx/listeners/Observatory/Observatory.hpp
+++ cxx/listeners/Observatory/Observatory.hpp
@@ -39,12 +39,28 @@ class LogItemDelegate : public QStyledItemDelegate {
 public:
     LogItemDelegate() = default;
 
+    /**
+     * @brief Convert item variant into text - returns strings and converts QDateTime into a readable timestamp with seconds
+     *
+     * @param value QVariant holding the value to be displayed
+     * @param locale Locale of the cell
+     *
+     * @return String representation of the item
+     */
     QString displayText(const QVariant& value, const QLocale& locale) const override;
 
 private:
+    /**
+     * @brief Paint the respective view using the color of the log level severity
+     *
+     * @param painter Pointer to the painter object
+     * @param option Style options
+     * @param index QModelIndex of the item to be painted
+     */
     void paint(QPainter* painter, const QStyleOptionViewItem& option, const QModelIndex& index) const override;
 
-    static inline std::map<constellation::log::Level, QColor> level_colors {
+    /** Color assignment for log levels */
+    static inline std::map<constellation::log::Level, QColor> level_colors_ {
         {constellation::log::Level::TRACE, QColor(224, 224, 224, 128)},
         {constellation::log::Level::DEBUG, QColor(200, 200, 200, 128)},
         {constellation::log::Level::INFO, QColor(191, 191, 191, 128)},
@@ -55,27 +71,85 @@ private:
     };
 };
 
+/**
+ * @class Observatory
+ * @brief Main window of the Observatory Logging UI
+ * @details This class implements the Qt QMainWindow component of the Observatory Logging UI, connects signals to
+ * the slots of different UI elements and takes care of updating the filter settings. Settings corresponding to UI elements
+ * are stored and retrieved again from file when restarting the UI.
+ */
 class Observatory : public QMainWindow, public Ui::wndLog {
     Q_OBJECT
 public:
+    /**
+     * @brief Observatory Constructor
+     *
+     * @param group_name Constellation group name to connect to
+     */
+
     Observatory(std::string_view group_name);
 
 public:
-    void closeEvent(QCloseEvent* /*event*/) override;
+    /**
+     * @brief Qt QCloseEvent handler which stores the UI settings to file
+     *
+     * @param event The Qt close event
+     */
+    void closeEvent(QCloseEvent* event) override;
 
 private slots:
+
+    /**
+     * @brief Private slot for changes of the global subscription level setting
+     *
+     * @param index New index of the global subscription level
+     */
     void on_globalLevel_currentIndexChanged(int index);
+
+    /**
+     * @brief Private slot for changes of the filter log level setting
+     *
+     * @param index New index of the filter log level
+     */
     void on_filterLevel_currentIndexChanged(int index);
+
+    /**
+     * @brief Private slot for changes of the sender filter
+     *
+     * @param text New text of the sender filter setting
+     */
     void on_filterSender_currentTextChanged(const QString& text);
+
+    /**
+     * @brief Private slot for changes of the topic filter
+     *
+     * @param text New text of the topic filter setting
+     */
     void on_filterTopic_currentTextChanged(const QString& text);
+
+    /**
+     * @brief Private slot for the "editing finished" signal from the message regex pattern
+     */
     void on_filterMessage_editingFinished();
+
+    /**
+     * @brief Private slot for "Clear" button of the filter settings
+     */
     void on_clearFilters_clicked();
 
+    /**
+     * @brief Private slot for selecting items from the log view and displaying a dialog with details
+     */
     void on_viewLog_activated(const QModelIndex& i);
 
 private:
+    /** Subscription pool listening to new log messages */
     QLogListener log_listener_;
+
+    /** Sorting and filtering proxy for displaying log messages */
     QLogFilter log_filter_;
+
+    /** Item delegate for painting log message rows in the view */
     LogItemDelegate log_message_delegate_;
 
     /** UI Settings */
