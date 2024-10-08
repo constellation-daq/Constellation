d41d55097f47001d46cdde57a4eb70409f844471
Simon Spannagel <simon.spannagel@desy.de>
2024-10-08 22:22:00 +0200

    Katherine: try soft command stop

--- cxx/satellites/Katherine/KatherineSatellite.cpp
+++ cxx/satellites/Katherine/KatherineSatellite.cpp
@@ -357,9 +357,14 @@ void KatherineSatellite::stopping() {
     // Read the current acquisition state:
     if(!acquisition_->aborted()) {
         const std::lock_guard device_lock {katherine_cmd_mutex_};
-        // Calls katherine_acq_abort which sends the stop command via CTRL UDP
-        acquisition_->abort();
-        LOG(DEBUG) << "Aborted acquisition";
+        // Calls katherine_acq_abort or katherine_acq_stop which sends the stop command via CTRL UDP
+        if(decode_data_) {
+            acquisition_->stop();
+        } else {
+            // When we do not decode the data, we need to abort hard
+            acquisition_->abort();
+        }
+        LOG(DEBUG) << "Stopped acquisition";
     }
 
     // Wait for acquisition task to finish, i.e. after all current measurement data has been processed and the
