62c2dc49cd7d5d140203c4e5cd0048cd024ab830
Simon Spannagel <simon.spannagel@desy.de>
2024-10-08 11:52:31 +0200

    CI: disable Qt components for some runners

---
+++ scripts/meson_disable_qt_components.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+# SPDX-FileCopyrightText: 2024 DESY and the Constellation authors
+# SPDX-License-Identifier: CC0-1.0
+
+# Disable all UI components which require Qt
+echo "-Dcontroller_missioncontrol=false"
