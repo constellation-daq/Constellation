3f5fe4ca0264001e6fff36b2baa8fd8484449d31
Stephan Lachnit <stephanlachnit@debian.org>
2024-10-13 14:41:52 +0200

    Keithley: do not allow request/metrics in ERROR state

--- python/constellation/satellites/Keithley/Keithley.py
+++ python/constellation/satellites/Keithley/Keithley.py
@@ -139,7 +139,7 @@ class Keithley(Satellite):
         return f"Device {is_str} in compliance", in_compliance, {}
 
     def _in_compliance_is_allowed(self, request: CSCPMessage) -> bool:
-        return self.fsm.current_state_value not in [SatelliteState.NEW]
+        return self.fsm.current_state_value not in [SatelliteState.NEW, SatelliteState.ERROR]
 
     @cscp_requestable
     def read_output(self, request: CSCPMessage) -> tuple[str, Any, dict]:
@@ -148,23 +148,23 @@ class Keithley(Satellite):
         return f"Output: {voltage}V, {current}A", {"voltage": voltage, "current": current, "timestamp": timestamp}, {}
 
     def _read_output_is_allowed(self, request: CSCPMessage) -> bool:
-        return self.fsm.current_state_value not in [SatelliteState.NEW]
+        return self.fsm.current_state_value not in [SatelliteState.NEW, SatelliteState.ERROR]
 
     @schedule_metric("V", MetricsType.LAST_VALUE, 5)
-    def Voltage(self) -> Any:
-        if self.fsm.current_state_value not in [SatelliteState.NEW]:
+    def VOLTAGE(self) -> Any:
+        if self.fsm.current_state_value not in [SatelliteState.NEW, SatelliteState.ERROR]:
             return self.device.read_output()[0]
         return None
 
     @schedule_metric("A", MetricsType.LAST_VALUE, 5)
-    def Current(self) -> Any:
-        if self.fsm.current_state_value not in [SatelliteState.NEW]:
+    def CURRENT(self) -> Any:
+        if self.fsm.current_state_value not in [SatelliteState.NEW, SatelliteState.ERROR]:
             return self.device.read_output()[1]
         return None
 
     @schedule_metric("", MetricsType.LAST_VALUE, 5)
-    def In_compliance(self) -> Any:
-        if self.fsm.current_state_value not in [SatelliteState.NEW]:
+    def IN_COMPLIANCE(self) -> Any:
+        if self.fsm.current_state_value not in [SatelliteState.NEW, SatelliteState.ERROR]:
             return self.device.in_compliance()
         return None
 
