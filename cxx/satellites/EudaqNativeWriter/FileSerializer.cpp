71c1cbd77d2865da0f6da65cab559edb65b2b514
Simon Spannagel <simon.spannagel@desy.de>
2024-10-22 11:20:35 +0200

    EudaqNativeWriter: use separate tag to mark trigger information

--- cxx/satellites/EudaqNativeWriter/FileSerializer.cpp
+++ cxx/satellites/EudaqNativeWriter/FileSerializer.cpp
@@ -95,9 +95,11 @@ void EudaqNativeWriterSatellite::FileSerializer::serialize_header(const constell
                                                                   std::uint32_t flags) {
     LOG(DEBUG) << "Writing event header";
 
-    // If we have a trigger number tag set, also add the corresponding flag:
-    const auto trigger_number_it = tags.find("trigger_number");
-    flags |= trigger_number_it != tags.end() ? std::to_underlying(EUDAQFlags::TRIGGER) : 0;
+    // If we have a trigger flag set, also add the corresponding EUDAQ flag:
+    const auto trigger_flag_it = tags.find("trigger_flag");
+    if(trigger_flag_it != tags.end()) {
+        flags |= trigger_flag_it->second.get<bool>() ? std::to_underlying(EUDAQFlags::TRIGGER) : 0;
+    }
 
     // Type, version and flags
     write_int(cstr2hash("RawEvent"));
@@ -112,6 +114,7 @@ void EudaqNativeWriterSatellite::FileSerializer::serialize_header(const constell
 
     // Downcast event sequence for message header, use the same for trigger number
     write_int(static_cast<std::uint32_t>(header.getSequenceNumber()));
+    const auto trigger_number_it = tags.find("trigger_number");
     write_int(trigger_number_it != tags.end() ? trigger_number_it->second.get<std::uint32_t>()
                                               : static_cast<std::uint32_t>(header.getSequenceNumber()));
 
