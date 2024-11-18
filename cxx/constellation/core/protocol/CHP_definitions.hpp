1048fccf83098a17327bb3270e85b595ec9acdff
Simon Spannagel <simon.spannagel@cern.ch>
2024-11-18 09:06:40 +0100

    cxx/core/chp: add flags for messages

--- cxx/constellation/core/protocol/CHP_definitions.hpp
+++ cxx/constellation/core/protocol/CHP_definitions.hpp
@@ -16,4 +16,28 @@ namespace constellation::protocol::CHP {
     /** Default lives for a remote on detection/replenishment */
     static constexpr std::uint8_t Lives = 3;
 
+    /** Possible CHP message flags */
+    enum class MessageFlags : std::uint8_t {
+        NONE = 0x0,
+
+        /* Indicate a extrasystole message */
+        IS_EXTRASYSTOLE = 0x1,
+
+        /* Indicate whether the current state has been reached autonomously or by CSCP command */
+        IS_AUTONOMOUS = 0x2,
+
+        /* Indicate that this message contains a status text */
+        HAS_STATUS = 0x4,
+    };
+
+    constexpr MessageFlags operator|(MessageFlags lhs, MessageFlags rhs) {
+        return static_cast<MessageFlags>(static_cast<std::underlying_type_t<MessageFlags>>(lhs) |
+                                         static_cast<std::underlying_type_t<MessageFlags>>(rhs));
+    }
+
+    constexpr bool operator&(MessageFlags lhs, MessageFlags rhs) {
+        return static_cast<bool>(static_cast<std::underlying_type_t<MessageFlags>>(lhs) &
+                                 static_cast<std::underlying_type_t<MessageFlags>>(rhs));
+    }
+
 } // namespace constellation::protocol::CHP
