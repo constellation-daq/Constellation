10860d668f1960674998c8c2f6f4642b509795a8
Simon Spannagel <simon.spannagel@cern.ch>
2024-11-25 16:24:41 +0100

    cxx/core/log: catch ZMQ error when sending stat

--- cxx/constellation/core/log/CMDPSink.cpp
+++ cxx/constellation/core/log/CMDPSink.cpp
@@ -225,6 +225,10 @@ void CMDPSink::sinkMetric(MetricValue metric_value) {
     // Lock the mutex - automatically done for regular logging:
     const std::lock_guard<std::mutex> lock {mutex_};
 
-    // Create and send CMDP message
-    CMDP1StatMessage(std::move(msghead), std::move(metric_value)).assemble().send(pub_socket_);
+    try {
+        // Create and send CMDP message
+        CMDP1StatMessage(std::move(msghead), std::move(metric_value)).assemble().send(pub_socket_);
+    } catch(const zmq::error_t& e) {
+        throw NetworkError(e.what());
+    }
 }
