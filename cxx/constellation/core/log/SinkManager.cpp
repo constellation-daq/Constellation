d9b03cda24c65e0f16b3391e0c86bae3e1b5f824
Simon Spannagel <simon.spannagel@desy.de>
2025-01-10 11:51:07 +0100

    cxx/core/cmdp: prepare sink manager for notifications

--- cxx/constellation/core/log/SinkManager.cpp
+++ cxx/constellation/core/log/SinkManager.cpp
@@ -245,3 +245,11 @@ void SinkManager::updateCMDPLevels(Level cmdp_global_level, std::map<std::string
         calculate_log_level(logger);
     }
 }
+
+void SinkManager::sendMetricNotification() {
+    cmdp_sink_->sinkNotification("STAT?", {});
+}
+
+void SinkManager::sendLogNotification() {
+    cmdp_sink_->sinkNotification("LOG?", {});
+}
