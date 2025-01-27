37f1be930ab94dfce7b6908de29738911ff37113
Stephan Lachnit <stephanlachnit@debian.org>
2025-01-27 09:44:36 +0100

    cxx: no need to cast as string

--- cxx/constellation/satellite/TransmitterSatellite.cpp
+++ cxx/constellation/satellite/TransmitterSatellite.cpp
@@ -126,7 +126,7 @@ void TransmitterSatellite::starting_transmitter(std::string_view run_identifier,
     run_metadata_ = {};
     mark_run_tainted_ = false;
     set_run_metadata_tag("version", CNSTLN_VERSION);
-    set_run_metadata_tag("version_full", std::string("Constellation ") + CNSTLN_VERSION_FULL);
+    set_run_metadata_tag("version_full", "Constellation " CNSTLN_VERSION_FULL);
     set_run_metadata_tag("run_id", run_identifier);
     set_run_metadata_tag("time_start", std::chrono::system_clock::now());
 
