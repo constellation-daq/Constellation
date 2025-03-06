d3b7165864b8d26198d7e029e59ee9433492c0a6
Simon Spannagel <simon.spannagel@desy.de>
2025-03-06 13:02:38 +0100

    Observatory: allow selection of log topics per sender

--- cxx/listeners/Observatory/QSenderSubscriptions.cpp
+++ cxx/listeners/Observatory/QSenderSubscriptions.cpp
@@ -17,15 +17,46 @@
 
 using namespace constellation;
 
-QSenderSubscriptions::QSenderSubscriptions(QWidget* parent,
-                                           const QString& name,
-                                           std::function<void(const std::string&, const std::string&, log::Level)> callback)
-    : QWidget(parent), name_(name), ui_(new Ui::QSenderSubscriptions), callback_(std::move(callback)) {
+QSenderSubscriptions::QSenderSubscriptions(
+    QWidget* parent,
+    const QString& name,
+    std::function<void(const std::string&, const std::string&, log::Level)> sub_callback,
+    std::function<void(const std::string&, const std::string&)> unsub_callback)
+    : QWidget(parent), name_(name), ui_(new Ui::QSenderSubscriptions), sub_callback_(std::move(sub_callback)),
+      unsub_callback_(std::move(unsub_callback)) {
     ui_->setupUi(this);
     ui_->senderName->setText(name_);
+    ui_->topicsView->setModel(&topics_);
+
+    // Connect selection change from topics list:
+    connect(ui_->topicsView->selectionModel(),
+            &QItemSelectionModel::selectionChanged,
+            this,
+            [&](const QItemSelection& selected, const QItemSelection& deselected) {
+                for(const auto& idx : selected.indexes()) {
+                    const auto topic = topics_.itemFromIndex(idx)->text().toStdString();
+                    sub_callback_(name_.toStdString(), topic, log::Level::TRACE);
+                }
+                for(const auto& idx : deselected.indexes()) {
+                    const auto topic = topics_.itemFromIndex(idx)->text().toStdString();
+                    unsub_callback_(name_.toStdString(), topic);
+                }
+            });
 }
 
 void QSenderSubscriptions::on_senderLevel_currentIndexChanged(int index) {
     const auto lvl = log::Level(index);
-    callback_(name_.toStdString(), "", lvl);
+    sub_callback_(name_.toStdString(), "", lvl);
+}
+
+void QSenderSubscriptions::setTopics(const QStringList& topics) {
+
+    // Remove old topics
+    topics_.clear();
+
+    // Add new topics
+    for(const auto& topic : topics) {
+        auto* item = new QStandardItem(topic);
+        topics_.appendRow(item);
+    }
 }
