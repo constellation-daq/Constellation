1369b25a930bebaa953f73bf74b67f3097204570
Simon Spannagel <simon.spannagel@cern.ch>
2025-03-07 07:35:18 +0100

    Observatory: drop widgets for disconnected senders

--- cxx/listeners/Observatory/QLogListener.hpp
+++ cxx/listeners/Observatory/QLogListener.hpp
@@ -86,6 +86,8 @@ signals:
      */
     void newSender(QString sender);
 
+    void disconnectedSender(QString sender);
+
     void newGlobalTopics(QStringList topics);
     void newSenderTopics(QString sender, QStringList topics);
 
@@ -104,6 +106,7 @@ private:
 
     void topics_changed(std::string_view sender) override;
     void sender_connected(std::string_view sender) override;
+    void sender_disconnected(std::string_view sender) override;
 
 private:
     /** Log messages & access mutex*/
