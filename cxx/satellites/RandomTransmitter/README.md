c2a51a1fcc5288cbe3fe4fef0d24470c89028660
Stephan Lachnit <stephanlachnit@debian.org>
2025-03-30 13:30:57 +0200

    RandomTransmitter: allow using pre-generated data and larger seed range

--- cxx/satellites/RandomTransmitter/README.md
+++ cxx/satellites/RandomTransmitter/README.md
@@ -19,6 +19,7 @@ meson configure build -Dsatellite_random_transmitter=true
 
 | Parameter | Type | Description | Default Value |
 |-----------|------|-------------|---------------|
-| `seed` | Unsigned 8-bit integer | Seed for the random engine | Random |
+| `pregen` | Bool | Use pre-generated data | `false` |
+| `seed` | Unsigned 32-bit integer | Seed for the random engine | Random |
 | `frame_size` | Unsigned integer | Size of a data frame in bytes | `1024` |
 | `number_of_frames` | Unsigned integer | Number of data frames per data message | `1` |
--- cxx/satellites/RandomTransmitter/RandomTransmitterSatellite.cpp
+++ cxx/satellites/RandomTransmitter/RandomTransmitterSatellite.cpp
@@ -32,23 +32,29 @@ RandomTransmitterSatellite::RandomTransmitterSatellite(std::string_view type, st
     support_reconfigure();
 }
 
-std::uint8_t RandomTransmitterSatellite::generate_random_seed() {
+std::uint32_t RandomTransmitterSatellite::generate_random_seed() {
     std::random_device rng {};
-    return static_cast<std::uint8_t>(rng());
+    return static_cast<std::uint32_t>(rng());
 }
 
 void RandomTransmitterSatellite::initializing(Configuration& config) {
-    seed_ = config.get<std::uint8_t>("seed", generate_random_seed());
+    pregen_ = config.get<bool>("pregen", false);
+    seed_ = config.get<std::uint32_t>("seed", generate_random_seed());
     frame_size_ = config.get<std::uint64_t>("frame_size", 1024U);
     number_of_frames_ = config.get<std::uint32_t>("number_of_frames", 1U);
     LOG(STATUS) << "Initialized with seed " << to_string(seed_) << " and " << frame_size_
                 << " bytes per data frame, sending " << number_of_frames_ << " "
-                << (number_of_frames_ == 1 ? "frame" : "frames") << " per message";
+                << (number_of_frames_ == 1 ? "frame" : "frames") << " per message"
+                << " with " << (pregen_ ? "pre" : "rng") << "-generated data";
 }
 
 void RandomTransmitterSatellite::reconfiguring(const Configuration& partial_config) {
+    if(partial_config.has("pregen")) {
+        pregen_ = partial_config.get<bool>("pregen");
+        LOG(STATUS) << "Reconfigured to using " << (pregen_ ? "pre" : "rng") << "-generated data";
+    }
     if(partial_config.has("seed")) {
-        seed_ = partial_config.get<std::uint8_t>("seed");
+        seed_ = partial_config.get<std::uint32_t>("seed");
         LOG(STATUS) << "Reconfigured seed: " << to_string(seed_);
     }
     if(partial_config.has("frame_size")) {
@@ -62,12 +68,21 @@ void RandomTransmitterSatellite::reconfiguring(const Configuration& partial_conf
 }
 
 void RandomTransmitterSatellite::starting(std::string_view run_identifier) {
-    byte_rng_.seed(seed_);
+    std::seed_seq seed_seq {seed_};
+    byte_rng_.seed(seed_seq);
     hwm_reached_ = 0;
     LOG(INFO) << "Starting run " << run_identifier << " with seed " << to_string(seed_);
 }
 
 void RandomTransmitterSatellite::running(const std::stop_token& stop_token) {
+    if(pregen_) {
+        running_pregen(stop_token);
+    } else {
+        running_rnggen(stop_token);
+    }
+}
+
+void RandomTransmitterSatellite::running_rnggen(const std::stop_token& stop_token) {
     while(!stop_token.stop_requested()) {
         auto msg = newDataMessage(number_of_frames_);
         for(std::uint32_t n = 0; n < number_of_frames_; ++n) {
@@ -77,6 +92,34 @@ void RandomTransmitterSatellite::running(const std::stop_token& stop_token) {
             std::ranges::generate(data, std::ref(byte_rng_));
             // Add data to message
             msg.addFrame(std::move(data));
+        }
+        const auto success = trySendDataMessage(msg);
+        if(!success) {
+            ++hwm_reached_;
+            LOG_N(WARNING, 5) << "Could not send message, skipping...";
+        }
+    }
+}
+
+void RandomTransmitterSatellite::running_pregen(const std::stop_token& stop_token) {
+    // Pre-generate random data
+    std::vector<std::vector<std::uint8_t>> frames {};
+    frames.reserve(number_of_frames_);
+    for(std::uint32_t n = 0; n < number_of_frames_; ++n) {
+        // Generate random bytes
+        std::vector<std::uint8_t> data {};
+        data.resize(frame_size_);
+        std::ranges::generate(data, std::ref(byte_rng_));
+        // Store data for later
+        frames.emplace_back(std::move(data));
+    }
+    LOG(INFO) << "Generation of random data complete";
+    // Actual sending loop
+    while(!stop_token.stop_requested()) {
+        auto msg = newDataMessage(frames.size());
+        for(const auto& frame : frames) {
+            // Copy vector to frame
+            msg.addFrame({std::vector(frame)});
         }
         const auto success = trySendDataMessage(msg);
         if(!success) {
