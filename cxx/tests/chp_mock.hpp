f86911d61527d0bd268d4e6dc7657862410edbd0
Simon Spannagel <simon.spannagel@desy.de>
2025-04-25 18:23:28 +0200

    cxx/tests: add heartbeat sending tests

--- cxx/tests/chp_mock.hpp
+++ cxx/tests/chp_mock.hpp
@@ -16,27 +16,57 @@
 #include <zmq.hpp>
 #include <zmq_addon.hpp>
 
+#include "constellation/core/chirp/Manager.hpp"
 #include "constellation/core/heartbeat/HeartbeatSend.hpp"
 #include "constellation/core/message/CHP1Message.hpp"
+#include "constellation/core/pools/SubscriberPool.hpp"
+#include "constellation/core/protocol/CHIRP_definitions.hpp"
 #include "constellation/core/protocol/CHP_definitions.hpp"
 #include "constellation/core/protocol/CSCP_definitions.hpp"
 
 #include "chirp_mock.hpp"
 
-// CHPSender with
-// state callback to internal state variable
-// send_extrasystole(state, status) that sets state and then calls sendExtrasystole
-// FIXME how to mock its CHIRP service, need to set manager before it starts pool/chirp manager
-
-class CHPSender : public constellation::heartbeat::HeartbeatSend {
+class CHPMockReceiver
+    : public constellation::pools::SubscriberPool<constellation::message::CHP1Message,
+                                                  constellation::protocol::CHIRP::ServiceIdentifier::HEARTBEAT> {
 public:
-    CHPSender(std::string name, std::chrono::milliseconds interval)
-        : HeartbeatSend(std::move(name), [&]() { return getState(); }, interval) {}
+    using SubscriberPoolT =
+        SubscriberPool<constellation::message::CHP1Message, constellation::protocol::CHIRP::ServiceIdentifier::HEARTBEAT>;
+    CHPMockReceiver()
+        : SubscriberPoolT("CHP", [this](constellation::message::CHP1Message&& msg) {
+              const std::lock_guard last_message_lock {last_message_mutex_};
+              last_message_ = std::make_shared<constellation::message::CHP1Message>(std::move(msg));
+              last_message_updated_.store(true);
+          }) {}
 
-    constellation::protocol::CSCP::State getState() const { return state_; }
+    void waitSubscription() {
+        while(!subscribed_.load()) {
+            using namespace std::chrono_literals;
+            std::this_thread::sleep_for(50ms);
+        }
+        subscribed_.store(false);
+    }
+    void waitNextMessage() {
+        while(!last_message_updated_.load()) {
+            using namespace std::chrono_literals;
+            std::this_thread::sleep_for(50ms);
+        }
+        last_message_updated_.store(false);
+    }
+    std::shared_ptr<constellation::message::CHP1Message> getLastMessage() {
+        const std::lock_guard last_message_lock {last_message_mutex_};
+        return last_message_;
+    }
 
-    void setState(constellation::protocol::CSCP::State state) { state_ = state; }
+protected:
+    void host_connected(const constellation::chirp::DiscoveredService& service) final {
+        subscribe(service.host_id, "");
+        subscribed_.store(true);
+    }
 
 private:
-    constellation::protocol::CSCP::State state_;
+    std::atomic_bool subscribed_ {false};
+    std::atomic_bool last_message_updated_ {false};
+    std::mutex last_message_mutex_;
+    std::shared_ptr<constellation::message::CHP1Message> last_message_;
 };
--- cxx/tests/test_core_chp_sender.cpp
+++ cxx/tests/test_core_chp_sender.cpp
@@ -20,19 +20,147 @@
 #include <catch2/matchers/catch_matchers_string.hpp>
 
 #include "constellation/core/protocol/CSCP_definitions.hpp"
+#include "constellation/core/utils/ManagerLocator.hpp"
+#include "constellation/core/utils/timers.hpp"
 
 #include "chp_mock.hpp"
 
+using namespace constellation::heartbeat;
 using namespace constellation::protocol;
+using namespace constellation::utils;
+using namespace std::chrono_literals;
 
-// NOLINTBEGIN(cert-err58-cpp,misc-use-anonymous-namespace)
+class CHPSender : public HeartbeatSend {
+public:
+    CHPSender(std::string name, std::chrono::milliseconds interval)
+        : HeartbeatSend(std::move(name), [&]() { return getState(); }, interval) {}
 
-TEST_CASE("Send and receive an extrasystole", "[chp][send]") {
-    // start chirp
+    CSCP::State getState() const { return state_; }
 
-    auto sender = CHPSender("dummy", std::chrono::milliseconds(10000));
-    sender.setState(CSCP::State::NEW);
-    sender.sendExtrasystole("test");
+    void setState(CSCP::State state) { state_ = state; }
+
+private:
+    CSCP::State state_ {CSCP::State::NEW};
+};
+
+TEST_CASE("Send a heartbeat", "[chp][send]") {
+    create_chirp_manager();
+
+    auto receiver = CHPMockReceiver();
+    receiver.startPool();
+
+    auto timer = StopwatchTimer();
+    const auto interval = std::chrono::milliseconds(300);
+    auto sender = CHPSender("Sender", interval);
+
+    // Mock service and wait until subscribed
+    const auto mocked_service = MockedChirpService("Sender", CHIRP::ServiceIdentifier::HEARTBEAT, sender.getPort());
+    receiver.waitSubscription();
+
+    // Wait for first message
+    receiver.waitNextMessage();
+
+    // Start timer and wait for the next:
+    timer.start();
+    receiver.waitNextMessage();
+    timer.stop();
+
+    // The delay should have been less than the configured interval:
+    REQUIRE(std::chrono::duration_cast<std::chrono::milliseconds>(timer.duration()) < interval);
+
+    // Check that heartbeat is decoded correctly
+    const auto last_message = receiver.getLastMessage();
+    REQUIRE(last_message->getSender() == "Sender");
+    REQUIRE(last_message->getState() == CSCP::State::NEW);
+    REQUIRE_FALSE(last_message->getStatus().has_value());
+    REQUIRE(last_message->getInterval() == interval);
+
+    receiver.stopPool();
+    ManagerLocator::getCHIRPManager()->forgetDiscoveredServices();
 }
 
-// NOLINTEND(cert-err58-cpp,misc-use-anonymous-namespace)
+TEST_CASE("Send an extrasystole", "[chp][send]") {
+    create_chirp_manager();
+
+    auto receiver = CHPMockReceiver();
+    receiver.startPool();
+
+    const auto interval = std::chrono::milliseconds(300);
+    auto sender = CHPSender("Sender", interval);
+
+    // Mock service and wait until subscribed
+    const auto mocked_service = MockedChirpService("Sender", CHIRP::ServiceIdentifier::HEARTBEAT, sender.getPort());
+    receiver.waitSubscription();
+
+    // Set state and send extrasystole
+    sender.setState(CSCP::State::RUN);
+    sender.sendExtrasystole("test");
+
+    // Wait until heartbeat is received
+    receiver.waitNextMessage();
+
+    // Check that extrasystole is decoded correctly
+    const auto extra_message = receiver.getLastMessage();
+    REQUIRE(extra_message->getSender() == "Sender");
+    REQUIRE(extra_message->getState() == CSCP::State::RUN);
+    REQUIRE(extra_message->getStatus().has_value());
+    REQUIRE(extra_message->getStatus().value() == "test");
+    REQUIRE(extra_message->getInterval() == interval);
+
+    // Wait until heartbeat is received
+    receiver.waitNextMessage();
+
+    // Check that heartbeat is decoded correctly
+    const auto next_message = receiver.getLastMessage();
+    REQUIRE(next_message->getSender() == "Sender");
+    REQUIRE(next_message->getState() == CSCP::State::RUN);
+    REQUIRE_FALSE(next_message->getStatus().has_value());
+    REQUIRE(next_message->getInterval() == interval);
+
+    receiver.stopPool();
+    ManagerLocator::getCHIRPManager()->forgetDiscoveredServices();
+}
+
+TEST_CASE("Change heartbeat interval", "[chp][send]") {
+    create_chirp_manager();
+
+    auto receiver = CHPMockReceiver();
+    receiver.startPool();
+
+    auto timer = StopwatchTimer();
+    auto sender = CHPSender("Sender", std::chrono::milliseconds(200));
+
+    // Mock service and wait until subscribed
+    const auto mocked_service = MockedChirpService("Sender", CHIRP::ServiceIdentifier::HEARTBEAT, sender.getPort());
+    receiver.waitSubscription();
+
+    // Wait for first message
+    receiver.waitNextMessage();
+
+    // Start timer and wait for the next:
+    timer.start();
+    receiver.waitNextMessage();
+    timer.stop();
+
+    // The delay should have been less than the configured interval:
+    REQUIRE(std::chrono::duration_cast<std::chrono::milliseconds>(timer.duration()) < std::chrono::milliseconds(200));
+
+    // Change interval:
+    sender.updateInterval(std::chrono::milliseconds(600));
+
+    // Wait for first message
+    receiver.waitNextMessage();
+
+    // Start timer and wait for the next:
+    timer.start();
+    receiver.waitNextMessage();
+    timer.stop();
+
+    // The delay should have been less than the new but more than the previous interval:
+    const auto duration = std::chrono::duration_cast<std::chrono::milliseconds>(timer.duration());
+    REQUIRE(duration > std::chrono::milliseconds(200));
+    REQUIRE(duration < std::chrono::milliseconds(600));
+
+    receiver.stopPool();
+    ManagerLocator::getCHIRPManager()->forgetDiscoveredServices();
+}
