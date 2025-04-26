261a8fe3470c3177d243d34334a4fd0043264b7a
Simon Spannagel <simon.spannagel@cern.ch>
2025-04-26 09:24:50 +0200

    python/heartbeater: implement CHP congestions control

--- python/constellation/core/heartbeater.py
+++ python/constellation/core/heartbeater.py
@@ -26,12 +26,18 @@ class HeartbeatSender(SatelliteStateHandler):
     ) -> None:
 
         super().__init__(name=name, interface=interface, **kwargs)
-        self.heartbeat_period = 1000
+        self.default_period = 60000
+        self.heartbeat_period = 500
+        self.subscribers = 0
 
         self.log_chp_s = self.get_logger("CHP")
 
         # register and start heartbeater
-        socket = self.context.socket(zmq.PUB)
+        socket = self.context.socket(zmq.XPUB)
+
+        # switch to verbose xpub mode to receive all subscription & unsubscription messages:
+        socket.setsockopt(zmq.XPUB_VERBOSER, True)
+
         if not hb_port:
             self.hb_port = socket.bind_to_random_port(f"tcp://{interface}")
         else:
@@ -54,6 +60,19 @@ class HeartbeatSender(SatelliteStateHandler):
         assert isinstance(self._com_thread_evt, threading.Event), "Thread Event not set up correctly"
         while not self._com_thread_evt.is_set():
             if ((datetime.now() - last).total_seconds() > self.heartbeat_period / 1000) or self.fsm.transitioned:
+                # Update number of subscribers and the associated heartbeat period
+                self.subscribers += self._hb_tm.parse_subscriptions()
+                self.heartbeat_period = min(
+                    self.default_period, int(self.default_period * pow(0.01 * self.subscribers, 2)) + 500
+                )
+                self.log_chp_s.trace(
+                    "Sending heartbeat, current period "
+                    + str(self.heartbeat_period)
+                    + "ms with "
+                    + str(self.subscribers)
+                    + " subscribers"
+                )
+
                 last = datetime.now()
                 state = self.fsm.current_state_value
                 self._hb_tm.send(state.value, int(self.heartbeat_period * 1.1))
