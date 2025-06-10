75d4610a045fdf64fb2f80b33ad4cc2a57624c58
Stephan Lachnit <stephanlachnit@debian.org>
2025-06-10 10:29:53 +0200

    python: fix interface argument choices

--- python/constellation/core/base.py
+++ python/constellation/core/base.py
@@ -20,7 +20,7 @@ import zmq
 from . import __version__, __version_code_name__
 from .cmdp import CMDPTransmitter
 from .logging import ConstellationLogger, ZeroMQSocketLogHandler
-from .network import get_interface_addresses, validate_interface
+from .network import get_interface_addresses_args, validate_interface
 
 
 @atexit.register
@@ -67,7 +67,7 @@ class ConstellationArgumentParser(ArgumentParser):
         self.network.add_argument(
             "--interface",
             type=validate_interface,
-            choices=get_interface_addresses(),
+            choices=get_interface_addresses_args(),
             default="*",
             help="The network interface (i.e. IP address) to bind to. "
             "Use '*' to bind to alla available interfaces "
