cf86c2c62ff46e7d92b0922556468fc5ef35f921
Stephan Lachnit <stephanlachnit@debian.org>
2025-06-17 11:20:36 +0200

    cxx: fix header include

--- cxx/constellation/satellite/BaseSatellite.cpp
+++ cxx/constellation/satellite/BaseSatellite.cpp
@@ -34,7 +34,6 @@
 #include "constellation/build.hpp"
 #include "constellation/core/config/Configuration.hpp"
 #include "constellation/core/config/Dictionary.hpp"
-#include "constellation/core/config/exceptions.hpp"
 #include "constellation/core/heartbeat/HeartbeatManager.hpp"
 #include "constellation/core/log/log.hpp"
 #include "constellation/core/message/CSCP1Message.hpp"
