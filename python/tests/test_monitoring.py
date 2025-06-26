d5bfb4194370f247ed92f8e070c149a4671c2c57
Stephan Lachnit <stephanlachnit@debian.org>
2025-06-26 16:09:50 +0200

    python: fix tests for CHIRPBeaconTransmitter

--- python/tests/test_monitoring.py
+++ python/tests/test_monitoring.py
@@ -14,7 +14,6 @@ from conftest import mock_packet_queue_sender, mocket, send_port
 from constellation.core.chirp import CHIRPBeaconTransmitter, CHIRPMessageType, CHIRPServiceIdentifier
 from constellation.core.cmdp import CMDPTransmitter, Metric, MetricsType
 from constellation.core.monitoring import MonitoringSender, ZeroMQSocketLogListener, schedule_metric
-from constellation.core.network import get_loopback_interface_name
 
 
 @pytest.fixture
@@ -183,7 +182,7 @@ def test_monitoring_file_writing(monitoringlistener, monitoringsender):
     ml, tmpdir = monitoringlistener
     ms = monitoringsender
     assert len(ml._log_listeners) == 0
-    chirp = CHIRPBeaconTransmitter("mock_sender", "mockstellation", interface=[get_loopback_interface_name()])
+    chirp = CHIRPBeaconTransmitter("mock_sender", "mockstellation", set(["127.0.0.1"]))
     chirp.broadcast(CHIRPServiceIdentifier.MONITORING, CHIRPMessageType.OFFER, send_port)
     # start metric sender thread
     ms._add_com_thread()
