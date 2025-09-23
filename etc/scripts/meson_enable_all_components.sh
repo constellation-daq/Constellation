556b25c27ff50d60f009a3286dd14a93467ad7bc
Stephan Lachnit <stephanlachnit@debian.org>
2025-09-23 15:18:36 +0200

    Move scripts to etc

--- etc/scripts/meson_enable_all_components.sh
+++ etc/scripts/meson_enable_all_components.sh
@@ -2,7 +2,7 @@
 # SPDX-FileCopyrightText: 2024 DESY and the Constellation authors
 # SPDX-License-Identifier: CC0-1.0
 
-MESON_OPTIONS_TXT="$(realpath $(dirname $0)/..)/meson_options.txt"
+MESON_OPTIONS_TXT="$(realpath $(dirname $0)/../..)/meson_options.txt"
 
 # Look for all options starting with satellite_ and enable them
 grep "option('satellite_" $MESON_OPTIONS_TXT | sed -E -e "s%option\('(satellite_\w+)'.*%\1%g" | sed -E -e "s%(.*)%-D\1=true%g" | paste -sd " " - | cat
