4833eadde1482c5d2a78d9b1df762fdcdcd97bd6
Simon Spannagel <simon.spannagel@cern.ch>
2025-12-20 23:28:28 +0100

    cxx/mc: disable default tooltip

--- cxx/constellation/gui/QController.cpp
+++ cxx/constellation/gui/QController.cpp
@@ -101,6 +101,15 @@ QVariant QController::get_data(std::map<std::string, Connection, std::less<>>::c
         return font;
     }
 
+    // Define tooltips
+    if(role == Qt::ToolTipRole) {
+        if(idx == 0 || idx == 1) {
+            return QString::fromStdString(name);
+        } else if(idx == 3) {
+            return QString::fromStdString(conn.last_message);
+        }
+    }
+
     // Below only handle display role
     if(role != Qt::DisplayRole) {
         return {};
