ca689ab07ab4dd5e331d6162334d74b8b54fd990
Stephan Lachnit <stephan.lachnit@desy.de>
2026-08-05 12:09:04 +0200

    Fix clang-tidy misc-include-cleaner

--- cxx/constellation/satellite/TransmitterSatellite.cpp
+++ cxx/constellation/satellite/TransmitterSatellite.cpp
@@ -11,7 +11,6 @@
 
 #include <chrono>
 #include <cstddef>
-#include <cstdint>
 #include <exception>
 #include <functional>
 #include <future>
--- cxx/satellites/Sputnik/SputnikSatellite.cpp
+++ cxx/satellites/Sputnik/SputnikSatellite.cpp
@@ -11,7 +11,6 @@
 
 #include <chrono>
 #include <cmath>
-#include <cstdint>
 #include <stop_token>
 #include <string_view>
 #include <thread>
