5f48de2939d4333386374d7ef3f8d8cb40255ffe
Simon Spannagel <simon.spannagel@cern.ch>
2026-08-30 23:03:24 +0200

    python: add helper to update the state

--- python/constellation/core/heartbeatchecker.py
+++ python/constellation/core/heartbeatchecker.py
@@ -217,12 +217,10 @@ class HeartbeatChecker(BaseSatelliteFrame):
                 hb.name = name
                 hb.refresh(timestamp.to_datetime())
                 state_enum = SatelliteState(state)
-                if state_enum != hb.state:
-                    old_state = hb.state
-                    hb.state = state_enum
-                    hb.last_statechange = datetime.now()
-                    hb.outdated = False
-                    self._on_state_change(hb.name, old_state, state_enum)
+
+                # Update the state
+                self._set_state(hb, state_enum)
+
                 self.log_chp.trace(
                     "%s reports state %s, flags %s%s, next message in %d",
                     name,
@@ -289,11 +287,7 @@ class HeartbeatChecker(BaseSatelliteFrame):
                             self.log_chp.warning(msg)
 
                             # Update state and invoke state change callback
-                            old_state = hb.state
-                            hb.state = SatelliteState.DEAD
-                            if old_state != SatelliteState.DEAD:
-                                hb.last_statechange = datetime.now(UTC)
-                                self._on_state_change(hb.name, old_state, SatelliteState.DEAD)
+                            self._set_state(hb, SatelliteState.DEAD)
 
                             # Check if the run needs to be marked as degraded
                             if hb.role.role_requires(CHPMessageFlags.MARK_DEGRADED):
@@ -322,6 +316,16 @@ class HeartbeatChecker(BaseSatelliteFrame):
 
     def _mark_degraded(self, reason: str) -> None:
         """Called when marking a run degraded"""
+
+    def _set_state(self, hb: HeartbeatState, state: SatelliteState) -> None:
+        if hb.state == state:
+            return
+
+        old_state = hb.state
+        hb.state = state
+        hb.outdated = False
+        hb.last_statechange = datetime.now(UTC)
+        self._on_state_change(hb.name, old_state, state)
 
     def _on_state_change(
         self,
